28e85ce830c33ec4e87948ad85f6e22610de3932
Anni <90419301+anniliu9@users.noreply.github.com>
2023-03-09 16:55:55 -0500

    Create Exercise2_Function_Help.py

---
+++ KagglePython/Exercise2_Function_Help.py
@@ -0,0 +1,4 @@
+# SETUP. You don't need to worry for now about what this code does or how it works.
+from learntools.core import binder; binder.bind(globals())
+from learntools.python.ex2 import *
+print('Setup complete.')
