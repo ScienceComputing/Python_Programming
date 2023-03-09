48abd2e042bbf35eef5180aa8c72c5901b068d01
Anni <90419301+anniliu9@users.noreply.github.com>
2023-03-09 16:58:58 -0500

    Update Exercise2_Function_Help.py

--- KagglePython/Exercise2_Function_Help.py
+++ KagglePython/Exercise2_Function_Help.py
@@ -2,3 +2,19 @@
 from learntools.core import binder; binder.bind(globals())
 from learntools.python.ex2 import *
 print('Setup complete.')
+
+## Question 1
+# Complete the body of the following function according to its docstring.
+def round_to_two_places(num):
+    """Return the given number rounded to two decimal places. 
+    
+    >>> round_to_two_places(3.14159)
+    3.14
+    """
+    return(round(num, 2))
+    # ("pass" is a keyword that does literally nothing. We used it as a placeholder
+    # because after we begin a code block, Python requires at least one line of code)
+    pass
+
+# Check your answer
+q1.check()
