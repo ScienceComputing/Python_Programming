390e3dbddfb017c71b7436225923a733e3cd046a
Anni <90419301+anniliu9@users.noreply.github.com>
2023-03-11 13:24:09 -0500

    Update Exercise2_Function_Help.py

--- KagglePython/Exercise2_Function_Help.py
+++ KagglePython/Exercise2_Function_Help.py
@@ -18,3 +18,15 @@ def round_to_two_places(num):
 
 # Check your answer
 q1.check()
+
+
+## Question 2
+# The help for round says that ndigits (the second argument) may be negative. What do you think will happen when it is? Try some examples in the following cell.
+round(338424, ndigits = -3)
+# As you've seen, ndigits=-1 rounds to the nearest 10, ndigits=-2 rounds to the nearest 100 and so on. Where might this be useful? 
+# Suppose we're dealing with large numbers:
+# The area of Finland is 338,424 km²
+# The area of Greenland is 2,166,086 km²
+# We probably don't care whether it's really 338,424, or 338,425, or 338,177. All those digits of accuracy are just distracting. We can chop them off by calling round() with ndigits=-3:
+# The area of Finland is 338,000 km²
+# The area of Greenland is 2,166,000 km²
