ec48f21df84a26075c8bb076adbffe92c4615e34
Anni <90419301+anniliu7@users.noreply.github.com>
2023-08-27 09:19:22 -0400

    Create Start_Unpacking.py

---
+++ Utilities/Start_Unpacking.py
@@ -0,0 +1,9 @@
+def drop_first_last(sequence_quality_score): 
+  first, *middle, last = sequence_quality_score
+  return avg(middle)
+
+record = ('Jane', 'Female', '12', '20', '0')
+name, gender, *PC_gene_expression = record
+name
+gender
+PC_gene_expression
