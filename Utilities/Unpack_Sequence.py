0f51b88b03497ebd2df55b0185bbada843618a86
Anni <90419301+anniliu7@users.noreply.github.com>
2023-08-27 09:11:42 -0400

    Update Unpack_Sequence.py

--- Utilities/Unpack_Sequence.py
+++ Utilities/Unpack_Sequence.py
@@ -2,3 +2,16 @@ data = [ 'XYZ', 90, 71.1, (2023, 12, 21) ]
 _, shares, price, _ = data
 shares
 price
+
+# Extended unpacking
+data = [ 'XYZ', 90, 71.1, (2023, 12, 21) ]
+_, shares, price, (year, *_) = data
+shares
+price
+year
+
+data = ( 'XYZ', 90, 71.1, (2023, 12, 21) )
+_, shares, price, (year, *_) = data
+shares
+price
+year
