821db53a7eb3d80f56cd7ee3ef0f1b1c4d45616f
Anni <90419301+anniliu7@users.noreply.github.com>
2023-08-27 09:32:50 -0400

    Update Unpack_Sequence.py

--- Utilities/Unpack_Sequence.py
+++ Utilities/Unpack_Sequence.py
@@ -15,3 +15,7 @@ _, shares, price, (year, *_) = data
 shares
 price
 year
+
+line = 'https://ftp.ncbi.nlm.nih.gov/geo/series/GSE85nnn/GSE85241/suppl/GSE85241%5Fcellsystems%5Fdataset%5F4donors%5Fupdated%2Ecsv%2Egz'
+ncbi, base_name,  = line.split('suppl/')
+base_name
