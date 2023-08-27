b5910d563d79c691ae8ba53b8770768e5b1f91e3
Anni <90419301+anniliu7@users.noreply.github.com>
2023-08-27 09:34:56 -0400

    Update Star_Unpacking.py

--- Utilities/Star_Unpacking.py
+++ Utilities/Star_Unpacking.py
@@ -2,7 +2,7 @@ def drop_first_last(sequence_quality_score):
   first, *middle, last = sequence_quality_score
   return avg(middle)
 
-record = ('Jane', 'Female', '12', '20', '0')
+record = ('Jane', 'Female', 12.890, 20.236, 0.899)
 name, gender, *PC_gene_expression = record
 name
 gender
