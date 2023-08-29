047b648fcea0d4e156711f0045b24b1b1c006896
Anni <90419301+anniliu7@users.noreply.github.com>
2023-08-29 18:46:30 -0400

    Create Keep_History.py

---
+++ Utilities/Keep_Last_N_Iterm.py
@@ -0,0 +1,14 @@
+from collections import deque
+def search(lines, pattern, history=6): 
+    old_lines = deque(maxlen=history) 
+    for line in lines:
+        if pattern in line:
+            yield line, old_lines 
+        old_lines.append(line)
+
+with open('trial.py') as f:
+    for line, old_lines in search(f, 'ATCCCCGGGGTTTTT', 6): 
+        for oline in old_lines:
+            print(oline, end='') 
+        print(line, end='') 
+        print('-'*20)
