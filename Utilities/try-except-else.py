083639032ad41d765253c8e1c931b9a172b36e43
Anni <90419301+anniliu7@users.noreply.github.com>
2023-08-30 19:36:04 -0400

    Create try-except-else.py

---
+++ Utilities/try-except-else.py
@@ -0,0 +1,24 @@
+nums = [1, 2, "A"]
+sum_nums = 0
+for item in nums:
+    try:
+        float_num = float(item)
+    except (ValueError, TypeError) as e:
+        print(f"This is a non-numeric item: {item}")
+    else:
+        sum_nums += float_num 
+        print(f"{sum_nums=}")
+
+# Bioinformatics senarior: load the BAM file and print reads in a specific region
+import pysam
+try:
+    bam_file = pysam.AlignmentFile("alignment.bam", "rb")
+except FileNotFoundError:
+    print("Error: The BAM file 'alignment.bam' was not found.")
+except (IOError, ValueError) as e:
+    print(f"Error: An error occurred while reading the BAM file: {e}")
+else:
+    for read in bam_file.fetch('chr8', 100, 120):
+        print(read)
+        
+    bam_file.close()
