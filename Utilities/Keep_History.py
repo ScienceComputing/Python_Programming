1c4646337b9480aa3b4b8f0f4b46561f85381a0b
Anni <90419301+anniliu7@users.noreply.github.com>
2023-08-29 20:37:15 -0400

    Update Keep_History.py

--- Utilities/Keep_History.py
+++ Utilities/Keep_History.py
@@ -12,3 +12,18 @@ with open('sequence_read.fasta') as f:
             print(oline, end='') 
         print(line, end='') 
         print('-'*20)
+
+# Bioinformatics senarior: calculate the moving average of the quality scores of base calls in DNA sequencing
+def calculate_moving_average(seq, window_size):
+    moving_average = deque()
+    window = deque(maxlen=window_size)
+    for value in seq:
+        window.append(value)
+        if len(window) == window_size:
+            avg = sum(window) / window_size
+            moving_average.append(avg)
+    return moving_average
+
+# Example usage:
+quality_scores = [30, 40, 25, 35, 45, 20, 30, 40, 50, 60]
+window_size = 3
