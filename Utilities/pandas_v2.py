2f460234bddbd2f8790eda49184c4eeaa462ebe8
Anni <90419301+anniliu7@users.noreply.github.com>
2023-09-07 21:22:33 -0400

    Create pandas_v2.py

---
+++ Utilities/pandas_v2.py
@@ -0,0 +1,31 @@
+# Compute built-in summary statistics
+df["column_name_1"].mean()
+df["column_name_1"].median()
+df["column_name_1"].mode()
+df["column_name_1"].min()
+df["column_name_1"].max()
+df["column_name_1"].var()
+df["column_name_1"].std()
+df["column_name_1"].sum()
+df["column_name_1"].quantile()
+
+# Example: find the oldest and youngest birth date
+df["birthdate"].min()
+df["birthdate"].max()
+
+# Compute custom summary statistics
+def pct90(column):
+    return column.quantile(90)
+
+def pct70(column):
+    return column.quantile(70)
+
+df["column_name_1"].agg(pct90)
+df[["column_name_1”, "column_name_2"]].agg(pct90)
+df["column_name_1"].agg([pct90, pct70])
+
+# Compute cumulative statistics - return an entire column, not a single number
+df["column_name_1"].cumsum()
+df["column_name_1"].cummax()
+df["column_name_1"].cummin()
+df["column_name_1"].cumprod()
