c640bb98d5d8c6572b5f042af6a5f508e8153950
Anni <90419301+anniliu7@users.noreply.github.com>
2023-09-09 07:29:47 -0400

    Update pandas_v2.py

--- Utilities/pandas_v2.py
+++ Utilities/pandas_v2.py
@@ -24,8 +24,19 @@ df["column_name_1"].agg(pct90)
 df[["column_name_1”, "column_name_2"]].agg(pct90)
 df["column_name_1"].agg([pct90, pct70])
 
+# Build a custom IQR function and mix it with np.median
+def iqr(column):
+    return column.quantile(0.75) - column.quantile(0.25)
+    
+df["column_name_1"].agg(iqr)
+
+import numpy as np
+print(df[["column_name_1", "column_name_2", "column_name_3"]].agg([iqr, np.median]))
+
 # Compute cumulative statistics - return an entire column, not a single number
 df["column_name_1"].cumsum()
 df["column_name_1"].cummax()
 df["column_name_1"].cummin()
 df["column_name_1"].cumprod()
+
+
