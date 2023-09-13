90e7ce25b3180815546ef9bcc0d4fd4c60dac8be
Anni <90419301+anniliu7@users.noreply.github.com>
2023-09-13 10:09:33 -0400

    Create enumerate.py

---
+++ Utilities/enumerate.py
@@ -0,0 +1,8 @@
+print(list(enumerate(range(100, 1001, 100))))
+# [(0, 100), (1, 200), (2, 300), (3, 400), (4, 500), (5, 600), (6, 700), (7, 800), (8, 900), (9, 1000)]`
+
+programming_language_list = ['Python', 'JavaScript', 'Julia', 'R', 'C']
+
+programming_language_enumerate = enumerate(programming_language_list)
+print(list(programming_language_enumerate))
+# [(0, 'Python'), (1, 'JavaScript'), (2, 'Julia'), (3, 'R'), (4, 'C')]
