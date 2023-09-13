e9726cc4a9f40584168348495d09f363a1c31923
Anni <90419301+anniliu7@users.noreply.github.com>
2023-09-13 08:12:58 -0400

    Update Vectorized_Math.py

--- Utilities/Vectorized_Math.py
+++ Utilities/Vectorized_Math.py
@@ -2,6 +2,9 @@ import time
 from collections import deque
 import numpy as np
 
+"""
+Case 1
+"""
 # The fatest vectorized solution
 t0 = time.time()
 data = np.array([9, 2, 7, 8, 10, 2, 7])
@@ -37,3 +40,26 @@ squared_data = [item**2 for item in data]
 
 t1 = time.time()
 print(t1 - t0)
+
+
+"""
+Case 2
+"""
+import numpy as np
+m = np.array([0,3,-9])
+n = np.array([0.2,8,7])
+
+# Do the inner product using for loop (not recommended due to its slow processing speed)
+inner_product_for_loop = 0
+for i in range(3):
+  inner_product_for_loop += m[i] * n[i]
+
+print(inner_product_for_loop)
+
+# Do the inner product using numpy
+inner_product_numpy = m.dot(n)
+
+assert inner_product_for_loop == inner_product_numpy
+
+# Interpret: numpy executes the code in precompiled C operation, which speeds up the running time for the code.
+# It is recommended to vectorize the code whenever possible.
