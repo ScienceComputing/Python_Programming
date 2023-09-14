8c2548de6111ecfb5e221bd323fbc971af13e79f
Anni <90419301+anniliu7@users.noreply.github.com>
2023-09-14 09:07:42 -0400

    Create lambda.py

---
+++ Utilities/lambda.py
@@ -0,0 +1,7 @@
+# lambda argument(s) : expression 
+lambda : print('Hello Anonymous Function!')
+greet = lambda : print('Hello Anonymous Function!')
+greet()
+
+greet_stock = lambda stock_name: print('Welcome to the world of', stock_name, 'stocks!')
+greet_stock('GOOGL')
