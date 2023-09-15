e35cfa8d11e8eca59c0ab72c7ae0d3becd0fdbab
Anni <90419301+anniliu7@users.noreply.github.com>
2023-09-15 16:40:39 -0400

    Create Create_SparkSession.py

---
+++ PySpark/Create_SparkSession.py
@@ -0,0 +1,9 @@
+# Import SparkSession from pyspark.sql
+from pyspark.sql import SparkSession
+
+# Make a new SparkSession called RNA_velocity_spark
+RNA_velocity_spark = SparkSession.builder.getOrCreate()
+# The SparkSession.builder.getOrCreate() method returns an existing SparkSession if there's already one in the environment, or creates a new one if necessary.
+
+# Print my_spark
+print(RNA_velocity_spark)
