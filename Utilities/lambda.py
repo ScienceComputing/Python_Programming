bd6ebe3f071fdaa2db6c613c5f3402d3966761dd
Anni <90419301+anniliu7@users.noreply.github.com>
2023-09-15 21:02:28 -0400

    Update lambda.py

--- Utilities/lambda.py
+++ Utilities/lambda.py
@@ -17,3 +17,22 @@ num_list = [6, 8, 15, 0, 90, 50]
 cubic_all = map(lambda num: num ** 3, num_list)
 print(cubic_all)
 print(list(cubic_all))
+
+# Bioinformatics scenarior: calculate the average expression level of a specific gene across all spatial locations. 
+gene_data = [
+    {'gene_name': 'GeneA', 'location1': 10, 'location2': 15, 'location3': 8},
+    {'gene_name': 'GeneB', 'location1': 5, 'location2': 12, 'location3': 7},
+    {'gene_name': 'GeneC', 'location1': 8, 'location2': 9, 'location3': 10},
+]
+
+# Define a lambda function to calculate the average expression of a gene
+calculate_average_expression = lambda gene_data_point: (
+    gene_data_point['location1'] + gene_data_point['location2'] + gene_data_point['location3']
+) / 3
+
+# Calculate the average expression of 'GeneA' using the lambda function
+gene_name_to_calculate = 'GeneA'
+average_expression = list(map(
+    calculate_average_expression, 
+    list(filter(lambda data_point: data_point['gene_name'] == gene_name_to_calculate, gene_data))
+))
