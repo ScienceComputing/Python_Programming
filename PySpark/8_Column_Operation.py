9caf483440fb907276b4a4a13267a0e0891e1c66
Anni <90419301+anniliu7@users.noreply.github.com>
2023-09-17 22:45:01 -0400

    Create 8_Column_Operation.py

---
+++ PySpark/8_Column_Operation.py
@@ -0,0 +1,8 @@
+# Create the Spark DataFrame
+RNA_velocity_train = RNA_velocity_spark.table("RNA_velocity_train")
+
+# Show the head
+RNA_velocity_train.show()
+
+# Add a new column
+RNA_velocity_train = RNA_velocity_train.withColumn("duration_hrs", RNA_velocity_train.time/60)
