7a1b5e1650ad6e45344adcabf69a64885ea22296
Anni <90419301+anniliu7@users.noreply.github.com>
2023-09-19 15:15:09 -0400

    Update IntegralDiscreteValue.py

--- DifferentialEquation/IntegralDiscreteValue.py
+++ DifferentialEquation/IntegralDiscreteValue.py
@@ -1,7 +1,6 @@
 # https://en.wikipedia.org/wiki/Euler–Maclaurin_formula
 
-# Case 1:
-# Integral on discrete values
+# Case 1: integral on discrete values
 m = lambda x: np.array([8,1,9,2,3])[x-1]
 m(5)
 m_int = lambda x_min, x_max: np.sum([m(x) for x in np.arange(x_min, x_max+1)])
