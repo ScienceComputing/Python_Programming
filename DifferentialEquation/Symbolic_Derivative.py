0dc0c49a0e23a545981dc71e452bad61f3c535c9
Anni <90419301+anniliu7@users.noreply.github.com>
2023-09-26 15:01:13 -0400

    Update Symbolic_Derivative.py

--- DifferentialEquation/Symbolic_Derivative.py
+++ DifferentialEquation/Symbolic_Derivative.py
@@ -6,12 +6,3 @@ diff(cos(x)*log(x), x)
                  cos(x)
 -log(x)⋅sin(x) + ──────
                    x
-
-# Solve an ode equation dx/dt + x = 6
-from sympy import dsolve, Eq, symbols, Function
-t = symbols('t')
-x = symbols('x', cls=Function)
-ode_eq = Eq(x(t).diff(t), 6 - x(t)) # Eq(Derivative(x(t), t), 6 - x(t))
-ode_sol = dsolve(ode_eq, x(t))
-ode_sol
-# Eq(x(t), C1*exp(-t) + 6)
