36cf90fb8ffcf08e6a9962427a73bf81bbf0d161
Anni <90419301+anniliu7@users.noreply.github.com>
2023-09-26 15:01:59 -0400

    Update Symbolic_Integral.py

--- DifferentialEquation/Symbolic_Integral.py
+++ DifferentialEquation/Symbolic_Integral.py
@@ -1,8 +1,22 @@
+# Case 1:
 from sympy import *
 x, t, z, nu = symbols('x t z nu')
 init_printing(use_unicode=True) # Ensure that all subsequent examples are formatted nicely using Unicode characters
+# Compute the indefinite integrals
 integrate(sin(x) + 6, x)
 # 6*x - cos(x)
 
-integrate(sin(x) + 6, (x, -1, 1)) # Compute the definite integrals
+# Compute the definite integrals
+integrate(sin(x) + 6, (x, -1, 1)) 
 # 12
+
+
+# Case 2:
+# Solve an ode equation dx/dt + x = 6
+from sympy import dsolve, Eq, symbols, Function
+t = symbols('t')
+x = symbols('x', cls=Function)
+ode_eq = Eq(x(t).diff(t), 6 - x(t)) # Eq(Derivative(x(t), t), 6 - x(t))
+ode_sol = dsolve(ode_eq, x(t))
+ode_sol
+# Eq(x(t), C1*exp(-t) + 6)
