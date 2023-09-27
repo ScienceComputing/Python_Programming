c36ea66f5b2b6fcd4df8b1d380481b573163e903
Anni <90419301+anniliu7@users.noreply.github.com>
2023-09-27 07:32:58 -0400

    Update pandas_v3.py

--- Utilities/pandas_v3.py
+++ Utilities/pandas_v3.py
@@ -10,6 +10,7 @@ df.reset_index()
 # Reset the row index as default and drop the previous index such that the previous index will not appear as a column
 df.reset_index(drop=True)
 
+# Slicing and subsetting using loc method
 # Subset the rows
 df[df["column_name_1"].isin(["value_1", "value_2"])] # This is a cumbersome way to select eligible rows
 df.set_index("column_name") # Set the variable to be filtered as the index
@@ -28,3 +29,26 @@ df.sort_index() # By default, this function sorts all index levels from outer to
 # Control the sorting level and order
 df.sort_index(level=["col_name_1", "col_name_2"], ascending=[True,False])
 df.sort_index(level="col_name_1")
+
+# Sort the row index before slicing
+df.set_index(["col_name_1", "col_name_2"]) .sort_index()
+df.loc["val_1":"val_3"] # Subset the outer index level that meet val_1, val_2, val_3; notice that the initial and final values are both included, compared to slicing a list
+# Special slicing feature for dates
+df.loc["2020-01-27":"2022-03-09"]
+df.loc["2020":"2022"]
+df.loc[("val_1_for_col_1", "val_1_for_col_2"):("val_3_for_col_1", "val_3_for_col_2")] # Subset the outer index level that meet val_1_for_col_1, val_2_for_col_1, val_3_for_col_1, and then subset the inner index level that meet val_1_for_col_2, val_2_for_col_2, val_3_for_col_2
+
+# Slice columns
+df.loc[:, "col_name_1":"col_name_3"] # Select columns beginning from col_name_1 to col_name_3, and keep all rows
+
+# Slice rows and columns at the same time
+df.loc[("val_1_for_col_1", "val_1_for_col_2", "val_1_for_col_3"):("val_3_for_col_1", "val_3_for_col_2", "val_3_for_col_3"), "col_name_1":"col_name_3"] 
+
+# Slicing and subsetting using iloc method, similar to slicing lists
+# Slice lists
+list_1[1:6] # Select the 2nd to 5th items in a list
+list_1[:6] # Select from the beginning to the 5th item in a list
+list_1[:] # Return the whole list
+
+# Subset the DataFrame by row and column numbers
+df.iloc[1:6, 1:6] # Select the 2nd to 5th rows and the 2nd to 5th columns
