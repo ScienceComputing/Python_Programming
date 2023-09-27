b9e64251ae33b0b9a3874bf3c876185e030bf0ac
Anni <90419301+anniliu7@users.noreply.github.com>
2023-09-27 07:06:02 -0400

    Update pandas_v3.py

--- Utilities/pandas_v3.py
+++ Utilities/pandas_v3.py
@@ -11,12 +11,12 @@ df.reset_index()
 df.reset_index(drop=True)
 
 # Subset the rows
-df[df["column_name_1"].isin(["value_1", "value_2"])] # This is a cumbersome way
-df.set_index("column_name") # We first set the variable to be filtered as the index
-df.loc[["value_1", "value_2"]] # We then use loc to filter on index values
+df[df["column_name_1"].isin(["value_1", "value_2"])] # This is a cumbersome way to select eligible rows
+df.set_index("column_name") # Set the variable to be filtered as the index
+df.loc[["value_1", "value_2"]] # We then use loc to filter on index values; here we select that rows that meet 2 values; Notice that .loc[] makes our code less burdensome to maintain and easy for our collaborators to read 
 df.loc["value_1"]
 
-# Set multiple columns as the row index => create multi-level/hierarchical indices 
+# Set multi-level/hierarchical indices made out of multiple columns when one category is nested inside another category
 df.set_index(["column_name_1", "column_name_2"]) # The index level of column_name_2 is nested inside of the index level of column_name_1
 # Subset the outer index level with a list
 df.loc[["value_1", "value_2"]]
@@ -24,6 +24,7 @@ df.loc[["value_1", "value_2"]]
 df.loc[[("val_1_for_col_1", "val_1_for_col_2"), ("val_2_for_col_1", "val_2_for_col_2")]]
 
 # Make the sorting by index values
-df.sort_index() # By default, this function sorts all index levels from outer to inner in ascending order
+df.sort_index() # By default, this function sorts all index levels from outer to inner in ascending order.
 # Control the sorting level and order
-df.sort_index(level=["col_name_1", "col_name_2"], ascending=[True, False])
+df.sort_index(level=["col_name_1", "col_name_2"], ascending=[True,False])
+df.sort_index(level="col_name_1")
