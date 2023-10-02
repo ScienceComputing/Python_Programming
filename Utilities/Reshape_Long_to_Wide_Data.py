acd96334cf1ff65de4c4e16b530261415d8ebe34
Anni <90419301+anniliu7@users.noreply.github.com>
2023-10-02 15:35:23 -0400

    Update and rename Long_to_Wide_Data.py to Reshape_Long_to_Wide_Data.py

--- Utilities/Reshape_Long_to_Wide_Data.py
+++ Utilities/Reshape_Long_to_Wide_Data.py
@@ -1,3 +1,3 @@
 # Reshape the long format data with 3 columns (barcode; gene; count) to a barcode x gene count matrix
-scRNA_raw_wide = scRNA_raw.pivot(index='barcode_id', columns='gene', values='count')
+scRNA_raw_wide = scRNA_raw.pivot(index='barcode_id', columns='gene', values='count', fill_value=0)
 scRNA_raw_wide.head()
