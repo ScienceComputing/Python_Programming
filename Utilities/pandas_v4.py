d9d5a6cf20bef746cbe8c55b1936524eee2b89f2
Anni <90419301+anniliu7@users.noreply.github.com>
2023-10-04 08:33:33 -0400

    Create pandas_v4.py

---
+++ Utilities/pandas_v4.py
@@ -0,0 +1,21 @@
+# Topic: missing values (NaN)
+# Motivation: some python functions ignore missing data by default, and some functions cannot handle the missing data
+# Detect the missing values
+df.isna() # Return a Boolean for every single element in the dataset
+df.isna().any() # Return a Boolean for every column in the dataset
+df.isna().sum() # Return a count for missing values for every column in the dataset
+
+# Visualize the missing values across different variables using the bar plot 
+Import matplotlib.pyplot as plt
+df.isna().sum().plot(kind="bar")
+plot.show()
+
+# Remove the missing values
+df.dropna() # Remove the rows that contain missing values
+
+# Replace the missing values 
+df.fillna(0) # Notice the distribution of variables with the missing values may change after replacing the missing values with 0
+df["numerical_var_before_replace"].hist(bins=20)
+plt.show()
+df["numerical_var_after_replace"].hist(bins=20)
+plt.show()
