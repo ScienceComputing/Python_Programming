ed7e2df519f91b8995bb3c9d35382c4aaed353c7
Anni <90419301+ScienceComputing@users.noreply.github.com>
2023-10-05 12:36:46 -0400

    Update Unpack_Sequence.py

--- Utilities/Unpack_Sequence.py
+++ Utilities/Unpack_Sequence.py
@@ -1,3 +1,4 @@
+# Unpacking lists
 data = [ 'XYZ', 90, 71.1, (2023, 12, 21) ] 
 _, shares, price, _ = data
 shares
@@ -10,6 +11,7 @@ shares
 price
 year
 
+# Unpacking tuples
 data = ( 'XYZ', 90, 71.1, (2023, 12, 21) )
 _, shares, price, (year, *_) = data
 shares
