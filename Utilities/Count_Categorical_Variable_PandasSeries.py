a707b270463335c669c68842b80f5ab48b6a4a98
Anni <90419301+ScienceComputing@users.noreply.github.com>
2023-10-05 14:30:26 -0400

    Create Count_Categorical_Variable_PandasSeries.py

---
+++ Utilities/Count_Categorical_Variable_PandasSeries.py
@@ -0,0 +1,24 @@
+# Import pandas
+import pandas as pd
+
+# Import news data as DataFrame: df
+news_df = pd.read_csv("news.csv")  
+
+# Initialize an empty dictionary: langs_count
+langs_count = {}
+
+# Extract column from DataFrame: col
+langs_col = news_df['lang']
+
+# Iterate over lang column in DataFrame
+for entry in langs_col:
+
+    # If the language is in langs_count, add 1 
+    if entry in langs_count.keys():
+        langs_count[entry] += 1
+    # Else add the language to langs_count, set the value to 1
+    else:
+        langs_count.update({entry: 1})
+
+# Print the populated dictionary
+print(langs_count)
