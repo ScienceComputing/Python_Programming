502988988df9798da183c5474369c318309e49f4
Anni <90419301+ScienceComputing@users.noreply.github.com>
2023-10-07 22:01:04 -0400

    Update Nested_Function.py

--- Utilities/Nested_Function.py
+++ Utilities/Nested_Function.py
@@ -8,3 +8,13 @@ def outer(…):
         """…"""
         y = x ** 2  
     return … 
+
+# Scenario: do the same process a number of times within a function
+def mod3plus7(x1, x2, x3, …, xN)
+    """Returns the remainder plus 5 of three values."""
+    new_x1 = x1 % 3 + 7
+    new_x2 = x2 % 3 + 7
+    new_x3 = x3 % 3 + 7
+    …
+    new_xN = xN % 3 + 7
+    return (new_x1, new_x2, new_x3, …, xN)
