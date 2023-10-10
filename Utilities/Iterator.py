6aad1d20d5a86f6ee041fe4fe7b31032d497a165
Anni <90419301+ScienceComputing@users.noreply.github.com>
2023-10-10 00:35:40 -0400

    Update Iterator.py

--- Utilities/Iterator.py
+++ Utilities/Iterator.py
@@ -16,3 +16,14 @@ for character_i in a_long_string:
 # Iterate over a range sequence
 for number_i in range(0, 9):
     print(number_i)
+
+# Iterate over a dictionary
+aa_dict = {'Alanine': 'Ala', 'Cysteine': 'Cys'}
+for key, value in aa_dict.items():
+    print(key, value)
+
+# Iterate over an iterable
+word = 'T cell'
+it = iter(word)
+next(it)
+print(*it) # Print all elements in an iterator
