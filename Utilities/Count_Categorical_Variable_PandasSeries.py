8c7e648172cc46d8011d2d181711b46c85a850c8
Anni <90419301+ScienceComputing@users.noreply.github.com>
2023-10-10 08:14:24 -0400

    Update Count_Categorical_Variable_PandasSeries.py

--- Utilities/Count_Categorical_Variable_PandasSeries.py
+++ Utilities/Count_Categorical_Variable_PandasSeries.py
@@ -140,3 +140,19 @@ def count_entries_2(df, *args):
 
 multi_col_count = count_entries_2(df, 'lang', 'source')
 print(multi_col_count)
+
+# Method 4: if the given dataframe is very large
+counts_dict={}
+
+# Iterate over the file chunk by chunk
+for chunk in pd.read_csv('news.csv',chunksize=10):
+
+    # Iterate over the column in DataFrame
+    for entry in chunk['lang']:
+        if entry in counts_dict.keys():
+            counts_dict[entry] += 1
+        else:
+            counts_dict[entry] = 1
+
+# Print the populated dictionary
+print(counts_dict)
