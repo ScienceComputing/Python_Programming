ed89d6da6c25bcc489e51de975e2fd240c02a7a8
Anni <90419301+ScienceComputing@users.noreply.github.com>
2023-10-09 20:03:32 -0400

    Update List.py

--- Utilities/List.py
+++ Utilities/List.py
@@ -3,3 +3,15 @@ list_1 = ["a", 200, 300, "ccc", 500]
 list_1.reverse()
 print(list_1)
 # [500, 'ccc', 300, 200, 'a']
+
+# Construct a list comprehension
+# newlist = [expression for item in iterable if condition == True]
+aa = ['histidine', 'isoleucine', 'leucine', 'lysine', 'methionine', 'phenylalanine', 'threonine', 'tryptophan', 'valine']
+aa_target = [x for x in aa if "t" in x]
+print(aa_target)
+# ['histidine', 'methionine', 'threonine', 'tryptophan']
+
+aa_target_2 = [x if x != 'phenylalanine' else 'methionine' for x in aa]
+print(aa_target_2)
+# ['histidine', 'isoleucine', 'leucine', 'lysine', 'methionine', 'methionine', 'threonine', 'tryptophan', 'valine']
+
