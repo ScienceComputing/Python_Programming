b81a297fd9e310cf5e3822f63738dac4398ffb87
Anni <90419301+ScienceComputing@users.noreply.github.com>
2023-10-10 08:26:40 -0400

    Update List.py

--- Utilities/List.py
+++ Utilities/List.py
@@ -8,11 +8,19 @@ print(list_1)
 # It is a single line of code, and more efficient than a for loop.
 # Construct a list comprehension
 # newlist = [expression for item in iterable if condition == True]
+# Case 1:
+nums = [2, 8, 10, 1, 21, 60]
+nums_minus_1 = [num + 1 for num in nums]
+print(nums_minus_1) 
+# [3, 9, 11, 2, 22, 61]
+
+# Case 2:
 aa = ['histidine', 'isoleucine', 'leucine', 'lysine', 'methionine', 'phenylalanine', 'threonine', 'tryptophan', 'valine']
 aa_target = [x for x in aa if "t" in x]
 print(aa_target)
 # ['histidine', 'methionine', 'threonine', 'tryptophan']
 
+# Case 3:
 aa_target_2 = [x if x != 'phenylalanine' else 'methionine' for x in aa]
 print(aa_target_2)
 # ['histidine', 'isoleucine', 'leucine', 'lysine', 'methionine', 'methionine', 'threonine', 'tryptophan', 'valine']
