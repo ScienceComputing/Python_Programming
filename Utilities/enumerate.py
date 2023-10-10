e8bf291d19aa767b93fe3f35a0a996e9094811bf
Anni <90419301+ScienceComputing@users.noreply.github.com>
2023-10-10 00:49:44 -0400

    Update enumerate.py

--- Utilities/enumerate.py
+++ Utilities/enumerate.py
@@ -1,7 +1,15 @@
+# enumerate is a function that takes any iterable as argument, and returns an enumerate object, which consists of pairs containing the elements of the original iterable, along with their index within the iterable. 
 print(list(enumerate(range(100, 1001, 100))))
 # [(0, 100), (1, 200), (2, 300), (3, 400), (4, 500), (5, 600), (6, 700), (7, 800), (8, 900), (9, 1000)]`
 
 programming_language_list = ['Python', 'JavaScript', 'Julia', 'R', 'C']
+for index, value in enumerate(programming_language_list):
+    print(index, value)
+# 0 Python
+# 1 JavaScript
+# 2 Julia
+# 3 R
+# 4 C
 
 programming_language_enumerate = enumerate(programming_language_list)
 print(list(programming_language_enumerate))
