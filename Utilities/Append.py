0cf9f708166e3c077596e9ea993b26053e4fca9d
Anni <90419301+ScienceComputing@users.noreply.github.com>
2023-10-10 08:31:27 -0400

    Update Append.py

--- Utilities/Append.py
+++ Utilities/Append.py
@@ -5,3 +5,12 @@ for num in nums:
   
 print(new_nums)
 # [2, 8, 10, 1, 21, 60]
+
+
+pairs = []
+for num1 in range(5, 8):
+    for num2 in range(9, 12):
+        pairs.append((num1, num2))
+
+print(pairs)
+# [(5, 9), (5, 10), (5, 11), (6, 9), (6, 10), (6, 11), (7, 9), (7, 10), (7, 11)]
