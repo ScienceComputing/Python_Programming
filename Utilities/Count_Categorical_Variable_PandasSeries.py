f68e14ca884f2ce01c7430d812c3afa91ebc8894
Anni <90419301+ScienceComputing@users.noreply.github.com>
2023-10-09 22:57:04 -0400

    Update Count_Categorical_Variable_PandasSeries.py

--- Utilities/Count_Categorical_Variable_PandasSeries.py
+++ Utilities/Count_Categorical_Variable_PandasSeries.py
@@ -81,6 +81,35 @@ def count_entries(df, col_name='lang'):
 
 count_entries(news_df, 'lang_1')
 
+# Method 2.2:
+def count_entries(df, col_name='lang'):
+    """Return a dictionary with counts of occurrences as value for each key."""
+    
+    # Raise a ValueError if col_name is NOT in DataFrame
+    if col_name not in df.columns:
+        raise ValueError('The DataFrame does not have a ' + col_name + ' column.')
+
+    # Initialize an empty dictionary: cols_count
+    cols_count = {}
+    
+    # Extract column from DataFrame: col
+    col = df[col_name]
+    
+    # Iterate over the column in DataFrame
+    for entry in col:
+
+        # If entry is in cols_count, add 1
+        if entry in cols_count.keys():
+            cols_count[entry] += 1
+            # Else add the entry to cols_count, set the value to 1
+        else:
+            cols_count[entry] = 1
+        
+        # Return the cols_count dictionary
+    return cols_count
+
+count_entries(news_df, 'lang_1')
+
 # Method 3:
 # Count values in any arbitrary number of columns specified by the user
 def count_entries_2(df, *args):
