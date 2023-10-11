9b4e6331ec31bf9c5afa29403512b4a796f6a716
Anni <90419301+ScienceComputing@users.noreply.github.com>
2023-10-11 07:20:11 -0400

    Update Generator.py

--- Utilities/Generator.py
+++ Utilities/Generator.py
@@ -9,6 +9,12 @@ for num in num_gen:
 
 print(list(num_gen)) # Convert the generator a list
 
+# Calculate the length of each string element
+person_list = ['John', 'Katie', 'Angel', 'Julia', 'Peter']
+lengths = (len(person) for person in person_list)
+for value in lengths:
+    print(value)
+
 # Lazy evaluation whereby the evaluation of the expression is delayed until its value is needed.
 # We prefer the generator when working with extremely large sequences, as we don't want to store the entire list in memory, which is what comprehensions would do. We want to generate elements of the sequence on the fly.
 print(next(num_gen))
@@ -25,6 +31,7 @@ print(list(aa_target))
 
 # Build the generator function that produces the generator objects when called. 
 # This function yields a sequence of values instead of returning a single value, using the keyword `yield`.
+# Case 1:
 def yield_multiple_items():
     yield "The 1st item"
     yield "The 2nd item"  
@@ -37,6 +44,7 @@ print(next(example))
 print(next(example))
 print(next(example))
 
+# Case 2:
 def num_sequence(n):
     """Generate values from 0 to n."""
     i = 0
@@ -48,6 +56,17 @@ result = num_sequence(7)
 print(type(result)) # <class 'generator'>
 for item in result:
     print(item)
+
+# Case 3:
+person_list = ['John', 'Katie', 'Angel', 'Julia', 'Peter']
+# Define generator function get_lengths that allows users to input any list
+def get_lengths(input_list):
+    """Generator function that yields the length of the strings in input_list."""
+    for person in input_list:
+        yield len(person)
+
+for value in get_lengths(person_list):
+    print(value)
 
 # Bioinformatics scenarior: parse a very large genomic data file
 def parse_fasta(file_path):
