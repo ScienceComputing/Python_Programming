0c08ed71a9cfb83db85d83418fa0763556119ef2
Anni <90419301+ScienceComputing@users.noreply.github.com>
2023-10-10 21:25:21 -0400

    Update List.py

--- Utilities/List.py
+++ Utilities/List.py
@@ -24,14 +24,34 @@ pairs = [(num1, num2) for num1 in range(5, 8) for num2 in range(9, 12)]
 print(pairs)
 # [(5, 9), (5, 10), (5, 11), (6, 9), (6, 10), (6, 11), (7, 9), (7, 10), (7, 11)]
 
-# Case 3:
+# Case 4: nested list comprehensions
+# [[output expression] for iterator variable in iterable]
+# output expression is itself a list comprehension
+# Create a 10 x 10 matrix using a list of lists: matrix
+matrix = [[col for col in range(10)] for row in range(10)]
+
+# Print the matrix
+for row in matrix:
+    print(row)
+# [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+# [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+# [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+# [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+# [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+# [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+# [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+# [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+# [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+# [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+# Case 5:
 # newlist = [expression for item in iterable if condition == True]
 aa = ['histidine', 'isoleucine', 'leucine', 'lysine', 'methionine', 'phenylalanine', 'threonine', 'tryptophan', 'valine']
 aa_target = [x for x in aa if "t" in x]
 print(aa_target)
 # ['histidine', 'methionine', 'threonine', 'tryptophan']
 
-# Case 4: substitute phenylalanine with methionine
+# Case 6: substitute phenylalanine with methionine
 aa_target_2 = [x if x != 'phenylalanine' else 'methionine' for x in aa]
 print(aa_target_2)
 # ['histidine', 'isoleucine', 'leucine', 'lysine', 'methionine', 'methionine', 'threonine', 'tryptophan', 'valine']
