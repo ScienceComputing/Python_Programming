8dbed374b8b55e48c74e2b100c3edf78a28b875f
Anni <90419301+ScienceComputing@users.noreply.github.com>
2023-10-11 07:45:07 -0400

    Update pandas_create_save.py

--- Utilities/pandas_create_save.py
+++ Utilities/pandas_create_save.py
@@ -3,12 +3,13 @@ my_dict = {"key_1": val_1, "key_2": val_2, "key_3", val_3}
 my_dict["key_1] # Access values of a dictionary 
 
 # Create DataFrames from a list of dictionaries	- construct the df row by row
-list_of_dict = [{"col_1": val_11, "col_2", val_21"}, {"col_1": val_12, "col_2", val_22"}]
+# Link: Convert_List_to_Dictionary.py
+list_of_dict = [{"col_1": val_11, "col_2": val_21"}, {"col_1": val_12, "col_2": val_22"}]
 my_df = pd.DataFrame(list_of_dict)
 print(my_df)
 
 # ***Create DataFrames from a dictionary of lists - construct the df column by column 
-dict_of_list = {"col_1": [val_11, val_12], "col_2", [val_21, val_22]}
+dict_of_list = {"col_1": [val_11, val_12], "col_2": [val_21, val_22]}
 my_df = pd.DataFrame(list_of_dict)
 print(my_df)
 
