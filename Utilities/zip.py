323fff8c2bbcdcbfa8277e60a6620b312001d187
Anni <90419301+ScienceComputing@users.noreply.github.com>
2023-10-11 07:32:44 -0400

    Update zip.py

--- Utilities/zip.py
+++ Utilities/zip.py
@@ -1,4 +1,5 @@
 # zip accepts an arbitrary number of iterables and returns an iterator of tuples.
+# Case 1: 
 list1 = [1, 2, 3]
 list2 = ['a', 'b', 'c']
 zip_iterator = zip(list1, list2) # Create a zip iterator by using the zip function
@@ -16,5 +17,15 @@ for z1, z2 in zip(list1, list2):
 
 zip_iterator = zip(list1, list2)
 result1, result2 = zip(*zip_iterator)
-print(result1) # (1, 2, 3)
-print(result2) # ('a', 'b', 'c')
+print(result1) 
+# (1, 2, 3)
+print(result2) 
+# ('a', 'b', 'c')
+
+# Case 2: 
+list1 = [1, 2, 3]
+list2 = ['a', 'b', 'c']
+zip_iterator = zip(list1, list2)
+result_dict = dict(zip_iterator)
+print(result_dict)
+# {1: 'a', 2: 'b', 3: 'c'}
