37762ba2fd57d66a0a44a8a890f8cfd6ec2b3d7e
Anni <90419301+ScienceComputing@users.noreply.github.com>
2023-10-12 16:26:32 -0400

    Update Load_Data_in_Chunk.py

--- Utilities/Load_Data_in_Chunk.py
+++ Utilities/Load_Data_in_Chunk.py
@@ -1,4 +1,4 @@
-# Do the computational on the data of the specified chunk size
+# Do the computations on the data of the specified chunk size
 import pandas as pd
 total = 0
 for chunk in pd.read_csv('aa.csv', chunksize=500):
