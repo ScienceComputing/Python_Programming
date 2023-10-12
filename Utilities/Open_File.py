4f880d3594ddefeed4f44b444dd4cf42c309d501
Anni <90419301+ScienceComputing@users.noreply.github.com>
2023-10-12 16:28:44 -0400

    Update Open_File.py

--- Utilities/Open_File.py
+++ Utilities/Open_File.py
@@ -1,3 +1,24 @@
 with open('trial.py') as f:
     for line in f:
         print(line)
+
+def read_large_file(file_object):
+    """A generator function to read a large file."""
+
+    # Loop indefinitely until the end of the file
+    while True:
+        # Read a line from the file: data
+        line = file_object.readline()
+        # Break if this is the end of the file
+        if not line:
+            break
+        # Yield the line of data
+        yield line
+        
+# Open a connection to the file
+with open('large_data.csv') as file:
+    gen_file = read_large_file(file)
+    # Print the first three lines of the file
+    print(next(gen_file))
+    print(next(gen_file))
+    print(next(gen_file))
