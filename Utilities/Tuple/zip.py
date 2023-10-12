45fdcce64627b3ce957bc0a7d87772f1f27c8c7b
Anni <90419301+ScienceComputing@users.noreply.github.com>
2023-10-12 19:57:29 -0400

    Update zip.py

--- Utilities/Tuple/zip.py
+++ Utilities/Tuple/zip.py
@@ -1,19 +1,21 @@
 # zip accepts an arbitrary number of iterables and returns an iterator of tuples.
 # Case 1: 
-list1 = [1, 2, 3]
-list2 = ['a', 'b', 'c']
+list1 = ['cake1', 'cake2', 'cake3']
+list2 = ['Vanilla Cake', 'Red Velvet Cake', 'Black Forest Cake']
 zip_iterator = zip(list1, list2) # Create a zip iterator by using the zip function
-result_list = list(zip_iterator)
-print(result_list)
-# [(1, 'a'), (2, 'b'), (3, 'c')]
+full_list = list(zip_iterator)
+print(full_list)
+# [('cake1', 'Vanilla Cake'), ('cake2', 'Red Velvet Cake'), ('cake3', 'Black Forest Cake')]
+
+zip_iterator = zip(list1, list2)
 print(*zip_iterator) # Print all the elements
-# (1, 'a') (2, 'b') (3, 'c')
+# ('cake1', 'Vanilla Cake') ('cake2', 'Red Velvet Cake') ('cake3', 'Black Forest Cake')
 
 for z1, z2 in zip(list1, list2):
     print(z1, z2)
-# 1 a
-# 2 b
-# 3 c
+# cake1 Vanilla Cake
+# cake2 Red Velvet Cake
+# cake3 Black Forest Cake
 
 zip_iterator = zip(list1, list2)
 result1, result2 = zip(*zip_iterator)
