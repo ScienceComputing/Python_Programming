0e275b3e31d8c6fa5373ddc7e860f221e1e586c5
Anni <90419301+ScienceComputing@users.noreply.github.com>
2023-10-12 19:02:16 -0400

    Create Tuple_vs_List.py

---
+++ Utilities/Tuple/Tuple_vs_List.py
@@ -0,0 +1,6 @@
+# Tuples and lists hold the data in the order
+# We can access elements inside a tuple or a list with an index
+# We can use the unpacking to expand a tuple or a list into named variables that represent each element in a tuple or a list
+
+# Unlike lists, elements in the tuples are immutable -> ensure our data is not altered
+# Tuples are easier to process and more memory efficient than lists
