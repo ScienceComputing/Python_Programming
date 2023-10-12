2f81ecec5546d9e82da4977e04f6a0205f54906b
Anni <90419301+ScienceComputing@users.noreply.github.com>
2023-10-12 16:42:07 -0400

    Update Load_Data_in_Chunk.py

--- Utilities/Load_Data_in_Chunk.py
+++ Utilities/Load_Data_in_Chunk.py
@@ -1,10 +1,10 @@
 # Do the computations on the data of the specified chunk size
 import pandas as pd
 total = 0
-for chunk in pd.read_csv('aa.csv', chunksize=500):
+for chunk in pd.read_csv('aa.csv', chunksize=500): # 500 lines to read from the file per chunk
     total += sum(chunk['peak'])
 print(total)
 
-df_reader = pd.read_csv('aa.csv', chunksize=10) # df_reader is a generator
+df_reader = pd.read_csv('aa.csv', chunksize=10) # df_reader is a generator; 
 print(next(df_reader)) # Print the first chunk
 print(next(df_reader))
