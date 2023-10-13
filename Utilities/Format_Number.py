6e721d6ebb65da0885686e1ff52f0931c4441b58
Anni <90419301+ScienceComputing@users.noreply.github.com>
2023-10-13 12:39:57 -0400

    Update and rename Print_Very_Large_Number.py to Format_Number.py

---
+++ Utilities/Format_Number.py
@@ -0,0 +1,24 @@
+# Case 1:
+num = 100**10
+print(num)
+# 100000000000000000000
+print(f"{num:,}") 
+# 100,000,000,000,000,000,000
+
+type(f"{num:,}")
+# <class 'str'>
+type(print(f"{num:,}"))
+# <class 'NoneType'>
+# Notice that printing a value but not returning a value will result in <NoneType>.
+
+# Case 2:
+# f'{value:{width}.{precision}}'
+# value -> any expression that evaluates to a number
+# width -> the number of characters used in total to display, but if value needs more space than the width specifies, then the additional space is used.
+# precision -> the number of characters used after the decimal point
+num = 0.00000001
+print(num)
+# 1e-08
+print(f"{num:10.8f}") 
+# 0.00000001
+
--- Utilities/Print_Very_Large_Number.py
+++
@@ -1,9 +0,0 @@
-num = 100**10
-print(f"{num:,}") 
-# 100,000,000,000,000,000,000
-
-type(f"{num:,}")
-# <class 'str'>
-type(print(f"{num:,}"))
-# <class 'NoneType'>
-# Notice that printing a value but not returning a value will result in <NoneType>.
