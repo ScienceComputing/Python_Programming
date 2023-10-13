14bfa41c0e5e1cf58d3a69c8ee2f75d4a7cd0c0f
Anni <90419301+ScienceComputing@users.noreply.github.com>
2023-10-12 21:17:07 -0400

    Update Join.py

--- Utilities/String/Join.py
+++ Utilities/String/Join.py
@@ -1,3 +1,6 @@
 education_year = ['3.5', '7', '9', '10', '20'] # We must input the string element here
 print(', '.join(education_year))
 # 3.5, 7, 9, 10, 20
+
+print(f"The candidate education years are {', '.join(education_year[0:4])}, and {education_year[-1]}.")
+# The candidate education years are 3.5, 7, 9, 10, and 20.
