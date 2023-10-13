874601d873655b78280ad21f0b5ec677a7b37a9b
Anni <90419301+ScienceComputing@users.noreply.github.com>
2023-10-12 21:05:53 -0400

    Create Print_f.py

---
+++ Utilities/Print_f.py
@@ -0,0 +1,9 @@
+# Pair up the girl and boy names: pairs
+pairs = zip(gene_names, protein_names)
+
+# Iterate over pairs
+for rank, pair in enumerate(pairs):
+    # Unpack pair: girl_name, boy_name
+    gene_names, protein_names = pair
+    # Print the rank and names associated with each rank
+    print(f'Rank {rank+1}: gene_name} and {protein_name}')
