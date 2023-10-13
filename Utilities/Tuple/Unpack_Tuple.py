66d8cedb697b7779bb64eeea51632eed3e70db19
Anni <90419301+ScienceComputing@users.noreply.github.com>
2023-10-12 20:05:31 -0400

    Update Unpack_Tuple.py

--- Utilities/Tuple/Unpack_Tuple.py
+++ Utilities/Tuple/Unpack_Tuple.py
@@ -4,3 +4,18 @@ _, shares, price, (year, *_) = data
 shares
 price
 year
+
+list1 = ['cake1', 'cake2', 'cake3']
+list2 = ['Vanilla Cake', 'Red Velvet Cake', 'Black Forest Cake']
+full_list = list(zip(list1, list2))
+
+for cake_index, cake_type in full_list:
+    print(cake_index)
+    print(cake_type)
+  
+# cake1
+# Vanilla Cake
+# cake2
+# Red Velvet Cake
+# cake3
+# Black Forest Cake
