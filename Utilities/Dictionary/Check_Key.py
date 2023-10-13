d79e1c0d8b0403b1edba72e3a2f1942500335147
Anni <90419301+ScienceComputing@users.noreply.github.com>
2023-10-13 12:09:16 -0400

    Update Check_Key.py

--- Utilities/Dictionary/Check_Key.py
+++ Utilities/Dictionary/Check_Key.py
@@ -2,3 +2,9 @@
 amino_acid_galleries = {'Glutamine': 'Help the immune system function and normal brain function and digestion', 'Cysteine': 'Help prevent side effects due to drug reactions and toxic chemicals'}
 'Tyrosine' in amino_acid_galleries
 # False
+
+if 'Cysteine' in amino_acid_galleries:
+    print('Cysteine is in the database. Its function is: %s' % amino_acid_galleries['Cysteine'])
+else:
+    print('No this amino acid found in the databse.')
+# Cysteine is in the database. Its function is: Help prevent side effects due to drug reactions and toxic chemicals
