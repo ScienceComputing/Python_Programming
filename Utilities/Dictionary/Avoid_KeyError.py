b097cb3e87f745ad24a311654f73b141a2ccf5dd
Anni <90419301+ScienceComputing@users.noreply.github.com>
2023-10-13 09:05:23 -0400

    Update Avoid_KeyError.py

--- Utilities/Dictionary/Avoid_KeyError.py
+++ Utilities/Dictionary/Avoid_KeyError.py
@@ -1,3 +1,11 @@
-dict1.get('key1', 'Not Found')
+dict1.get('key1', 'N/A')
 
-# If the key is not in the dictionary, .get() will return 'Not Found'.
+# If the key is not in the dictionary, .get() will return 'N/A'.
+
+for key in dict1:
+    print(key, [dict1.get('subkey1', 'N/A') for item in dict1[key]])  
+
+dict1
+{'key1': [{'subkey1': 'Blue','subkey2': None}, {'subkey1': 'Gray', 'subkey2': 'Perfect'}],
+ 'key2': [{'subkey1': 'Red','subkey2': 'Great'}, {'subkey1': 'Green', 'subkey2': '...'}]
+}
