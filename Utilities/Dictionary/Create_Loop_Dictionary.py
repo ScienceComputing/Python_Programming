fc65bd4d843b5dba333157a29a9b25e5c9982901
Anni <90419301+ScienceComputing@users.noreply.github.com>
2023-10-13 12:02:23 -0400

    Update Create_Loop_Dictionary.py

--- Utilities/Dictionary/Create_Loop_Dictionary.py
+++ Utilities/Dictionary/Create_Loop_Dictionary.py
@@ -1,9 +1,22 @@
 # Method 1: {}
 amino_acid_galleries = {}
+gallery_info = [('Glutamine', 'Help the immune system function and normal brain function and digestion'), ('Cysteine', 'Help prevent side effects due to drug reactions and toxic chemicals')]
 
 # Turn tuples into a dictionary
 for name, function in gallery_info: # gallery_info is a list of tuples
     amino_acid_galleries[name] = function
+print(amino_acid_galleries)
+# {'Glutamine': 'Help the immune system function and normal brain function and digestion', 'Cysteine': 'Help prevent side effects due to drug reactions and toxic chemicals'}
+
+# Loop over a dictionary
+for name, function in amino_acid_galleries.items():
+    print(name)
+    print(function)
+
+# Glutamine
+# Help the immune system function and normal brain function and digestion
+# Cysteine
+# Help prevent side effects due to drug reactions and toxic chemicals
 
 # Find the last 5 amino acid names
 for name in sorted(amino_acid_galleries)[-5:]: # Loop over the key
