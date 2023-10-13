9f90cace4182bb0f7677286478c72aa21f48c575
Anni <90419301+ScienceComputing@users.noreply.github.com>
2023-10-13 12:15:39 -0400

    Update and rename Check_Key.py to Print_Check_Key.py

--- Utilities/Dictionary/Print_Check_Key.py
+++ Utilities/Dictionary/Print_Check_Key.py
@@ -1,5 +1,9 @@
-# Check if the key in exists in a dictionary
+# Print all keys in a dictionary
 amino_acid_galleries = {'Glutamine': 'Help the immune system function and normal brain function and digestion', 'Cysteine': 'Help prevent side effects due to drug reactions and toxic chemicals'}
+amino_acid_galleries.keys()
+# dict_keys(['Glutamine', 'Cysteine'])
+
+# Check if the key in exists in a dictionary
 'Tyrosine' in amino_acid_galleries
 # False
 
