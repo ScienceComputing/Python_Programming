04627e76f6d586d01aec4465ddc5719ba8a8d9b4
Anni <90419301+ScienceComputing@users.noreply.github.com>
2023-10-12 20:02:44 -0400

    Update and rename Utilities/Unpack_Sequence.py to Utilities/List/Unpack_List.py

--- Utilities/List/Unpack_List.py
+++ Utilities/List/Unpack_List.py
@@ -11,13 +11,6 @@ shares
 price
 year
 
-# Unpacking tuples
-data = ( 'XYZ', 90, 71.1, (2023, 12, 21) )
-_, shares, price, (year, *_) = data
-shares
-price
-year
-
 line = 'https://ftp.ncbi.nlm.nih.gov/geo/series/GSE85nnn/GSE85241/suppl/GSE85241%5Fcellsystems%5Fdataset%5F4donors%5Fupdated%2Ecsv%2Egz'
 ncbi, base_name = line.split('suppl/')
 base_name
