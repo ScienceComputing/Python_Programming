4610f6324ba295a3959e8df9ede3bee16f0e9ded
Anni <90419301+ScienceComputing@users.noreply.github.com>
2023-10-13 12:33:30 -0400

    Create Decimals.py

---
+++ Utilities/Decimals.py
@@ -0,0 +1,7 @@
+# Unlike float great for approximation and scientific notations, decimals have the exact precision and perfect for the currency operations
+from decimal import Decimal
+Decimal('12567')
+# Decimal('12567')
+
+float(12567968650494373629)
+# 1.2567968650494374e+19
