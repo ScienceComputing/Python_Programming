5bfe74486e7e6554d2e9e2b878cfdd4fcbc1b763
Anni <90419301+ScienceComputing@users.noreply.github.com>
2023-10-13 09:52:28 -0400

    Update Delete_KeyValue.py

--- Utilities/Dictionary/Delete_KeyValue.py
+++ Utilities/Dictionary/Delete_KeyValue.py
@@ -1,6 +1,6 @@
 # Method 1: del will throw a KeyError if the key to delete does not exist
-del amino_acid['cys']
+del amino_acid['Cys'] # Cys is a key
 
 # Method 2:
-amino_acid_cys = amino_acid.pop('cys') # Save the removed key-value pair
+amino_acid_cys = amino_acid.pop('Cys') # Save the removed key-value pair
 print(amino_acid_cys) 
