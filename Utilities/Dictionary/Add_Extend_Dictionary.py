73a5eab456e63257c97437d23867af71bc3fd7b6
Anni <90419301+ScienceComputing@users.noreply.github.com>
2023-10-13 08:56:44 -0400

    Update Add_Extend_Dictionary.py

--- Utilities/Dictionary/Add_Extend_Dictionary.py
+++ Utilities/Dictionary/Add_Extend_Dictionary.py
@@ -1,5 +1,8 @@
 amino_acid['new_key'] = value
 
+amino_acid = {'Glutamine': 'Help the immune system function and normal brain function and digestion'}
 amino_acid_cys = [('Cysteine', 'Help prevent side effects due to drug reactions and toxic chemicals')]
-amino_acid['cys'].update(new_amino_acid)
-print(amino_acid['cys'])
+
+# Update the dictionary with new key-value pair 
+amino_acid.update(amino_acid_cys)
+print(amino_acid['Cys'])
