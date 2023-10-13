1683d0431d7df0843154d47df2c9372dfdab052b
Anni <90419301+ScienceComputing@users.noreply.github.com>
2023-10-12 22:28:03 -0400

    Create Add_Extend_Dictionary.py

---
+++ Utilities/Dictionary/Add_Extend_Dictionary.py
@@ -0,0 +1,5 @@
+amino_acid['new_key'] = value
+
+amino_acid_cys = [('Cysteine', 'Help prevent side effects due to drug reactions and toxic chemicals')]
+amino_acid['cys'].update(new_amino_acid)
+print(amino_acid['cys'])
