9fd573c51962e50d8fa267305c076e7277baafe2
Anni <90419301+ScienceComputing@users.noreply.github.com>
2023-10-12 21:20:48 -0400

    Update Match_Part_String.py

--- Utilities/String/Match_Part_String.py
+++ Utilities/String/Match_Part_String.py
@@ -1,3 +1,6 @@
 cake_name = ['Vanilla Cake', 'Red Velvet Cake', 'Black Forest Cake']
 print([name for name in cake_name if name.startswith('B')])
 # ['Black Forest Cake']
+
+print([name for name in cake_name if name.startswith('b')])
+# []
