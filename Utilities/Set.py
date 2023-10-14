f1b8cfc55c22acc991cf69002bcb962aac231bc6
Anni <90419301+ScienceComputing@users.noreply.github.com>
2023-10-13 21:47:38 -0400

    Update Set.py

--- Utilities/Set.py
+++ Utilities/Set.py
@@ -1,5 +1,12 @@
 # Sets [unordered, mutable] are useful for finding all the unique values in a column of the data, a list of elements, or rows from a file.
 
+# Create a set
+donut_consumed = ['Vanilla Sprinkle', 'Chocolate Sprinkle', 'Powder', 'Powder', 'Chocolate Sprinkle']
+donut_type = set(donut_consumed)
+print(donut_type)
+# {'Vanilla Sprinkle', 'Chocolate Sprinkle', 'Powder'}
+
+
 # See the length of a set = number of unique elements
 print(len(set(['a', 'a', 'b', 'c']))) # 3
 
