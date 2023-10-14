edf482afa9d0e3a38acd1ad2177a40aa0df118eb
Anni <90419301+ScienceComputing@users.noreply.github.com>
2023-10-13 23:29:42 -0400

    Update defaultdict.py

--- Utilities/Dictionary/defaultdict.py
+++ Utilities/Dictionary/defaultdict.py
@@ -1,3 +1,26 @@
+# Conventional way to construct a multi-dictionary from a dictionary
+stock_data_list = {
+    'GOOG': [136.23, 135.21, 130.75, 137.25],
+    'AAPL': [150.25, 151.50, 149.75, 148.60],
+    'MSFT': [305.20, 307.40, 303.80, 305.60]
+}
+stock_data={}
+for symbol, price_list in stock_data_list.items():
+    if symbol not in stock_data:
+        stock_data[symbol] = [] # Initialize the first value
+    stock_data[symbol].extend(price_list)
+
+print(stock_data)
+
+# Smart way to construct a multi-dictionary from a dictionary
+stock_data=defaultdict(list)
+for symbol, price_list in stock_data_list.items():
+    stock_data[symbol].extend(price_list)
+
+print(stock_data)
+print(stock_data['GOOG'])
+
+
 # Create a multi-dictionary that maps keys to more than one value
 # If we are interested in preserving the insertion order of the items, we could build a list-based multi-dictionary
 stock_data_list = {
@@ -38,25 +61,3 @@ print(stock_data_set['GOOG'])
 # Caveat: defaultdict will automatically create values for non-exisitng keys
 stock_data_set['MSFT']
 print(stock_data_set)
-
-# Conventional way to construct a multi-dictionary from a dictionary
-stock_data_list = {
-    'GOOG': [136.23, 135.21, 130.75, 137.25],
-    'AAPL': [150.25, 151.50, 149.75, 148.60],
-    'MSFT': [305.20, 307.40, 303.80, 305.60]
-}
-stock_data={}
-for symbol, price_list in stock_data_list.items():
-    if symbol not in stock_data:
-        stock_data[symbol] = [] # Initialize the first value
-    stock_data[symbol].extend(price_list)
-
-print(stock_data)
-
-# More elegant way to construct a multi-dictionary from a dictionary
-stock_data=defaultdict(list)
-for symbol, price_list in stock_data_list.items():
-    stock_data[symbol].extend(price_list)
-
-print(stock_data)
-print(stock_data['GOOG'])
