363660cf2ce757e0adeeedece9caf61fda6d08cd
Anni <90419301+ScienceComputing@users.noreply.github.com>
2023-10-14 15:28:40 -0400

    Update String_Basic.py

--- Utilities/String/String_Basic.py
+++ Utilities/String/String_Basic.py
@@ -9,3 +9,12 @@ statement = "Number of characters in the name of this amino acid:"
 
 # Concatenate strings and print result
 print(statement+' '+to_string)
+
+# Select the first 3 characters
+amino_acid[:3]
+
+# Select from 3rd to the 6th character
+amino_acid[3:6]
+
+# Select from 3rd character to the end
+amino_acid[2:]
