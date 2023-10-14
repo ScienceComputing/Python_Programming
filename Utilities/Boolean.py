0f7b86231a21ce79615ee4ac38f585e997b06a27
Anni <90419301+ScienceComputing@users.noreply.github.com>
2023-10-13 21:27:45 -0400

    Update Boolean.py

--- Utilities/Boolean.py
+++ Utilities/Boolean.py
@@ -1,3 +1,6 @@
 out_of_donut = True
 if out_of_donut:
     print("Please run to the supermarket NOW!")
+
+# Truthy values: non 0; non-empty string; non-empty list; non-empty dictionary
+# Falsey valuees: 0; ""; []; {}; None
