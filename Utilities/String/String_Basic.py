eed68596e7a151eb7f0440feffff4e8fb502ca83
Anni <90419301+ScienceComputing@users.noreply.github.com>
2023-10-14 15:25:10 -0400

    Create String_Basic.py

---
+++ Utilities/String/String_Basic.py
@@ -0,0 +1,11 @@
+# Find characters in amino_acid variable
+length_string = len(amino_acid)
+
+# Convert to string
+to_string = str(length_string)
+
+# Predefined variable
+statement = "Number of characters in the name of this amino acid:"
+
+# Concatenate strings and print result
+print(statement+' '+to_string)
