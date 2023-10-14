cbab7ea295015ee6f39679edbb64452499b16fc1
Anni <90419301+ScienceComputing@users.noreply.github.com>
2023-10-13 23:42:39 -0400

    Update Add_Extend_Dictionary.py

--- Utilities/Dictionary/Add_Extend_Dictionary.py
+++ Utilities/Dictionary/Add_Extend_Dictionary.py
@@ -1,4 +1,11 @@
-amino_acid['new_key'] = value
+amino_acid['new_key'] = value 
+amino_acid['new_key'] = []
+amino_acid['new_key'] = ()
+amino_acid['new_key'] = {}
+amino_acid['new_key'].append(value)
+amino_acid['new_key'].append([])
+amino_acid['new_key'].append(())
+amino_acid['new_key'].append({})
 
 amino_acid = {'Glutamine': 'Help the immune system function and normal brain function and digestion'}
 amino_acid_cys = [('Cysteine', 'Help prevent side effects due to drug reactions and toxic chemicals')]
