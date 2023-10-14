4ce88f2ae225c1bf2bd3ddfbd25313dd138c48cc
Anni <90419301+ScienceComputing@users.noreply.github.com>
2023-10-14 14:09:34 -0400

    Update dataclass.py

--- Utilities/Class/dataclass.py
+++ Utilities/Class/dataclass.py
@@ -17,6 +17,7 @@ print(First_aa.name)
 print(First_aa.function)
 # Help prevent side effects due to drug reactions and toxic chemicals
 
+
 from dataclasses import asdict, astuple
 First_aa = Amino_acid('Cysteine', 'Help prevent side effects due to drug reactions and toxic chemicals')
 First_aa_dict = asdict(First_aa)
@@ -25,3 +26,22 @@ print(First_aa_dict)
 First_aa_tuple = astuple(First_aa)
 print(First_aa_tuple)
 # ('Cysteine', 'Help prevent side effects due to drug reactions and toxic chemicals')
+
+
+from dataclasses import dataclass
+from decimal import Decimal
+@dataclass
+class Price_entry:
+    # Define the fields on the class
+    food: str
+    quantity: int
+    unit_price: Decimal
+        
+    # Define a property that returns the total_price
+    @property
+    def Total_price(self):
+        return int(self.quantity) * self.unit_price
+
+Grazed_donut = Price_entry('Grazed_donut', 3, Decimal('2.5'))
+Grazed_donut.Total_price
+# Decimal('7.5')
