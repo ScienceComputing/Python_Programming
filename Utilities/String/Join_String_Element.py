9a72486dcb86e12f09f22a574192a55a65b6e262
Anni <90419301+ScienceComputing@users.noreply.github.com>
2023-10-14 20:14:34 -0400

    Create Join_String_Element.py

---
+++ Utilities/String/Join_String_Element.py
@@ -0,0 +1,5 @@
+# seperator.join(iterable)
+str1 = ['Cysteine', 'helps', 'prevent', 'side', 'effects', 'due', 'to', 'drug', 'reactions', 'and', 'toxic', 'chemicals']
+str2 = ' '.join(str1)
+print(str2)
+# Cysteine helps prevent side effects due to drug reactions and toxic chemicals
