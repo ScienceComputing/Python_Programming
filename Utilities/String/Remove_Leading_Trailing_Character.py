4f20310a8f51c6344ef7e15f7ceb4e64e201445e
Anni <90419301+ScienceComputing@users.noreply.github.com>
2023-10-14 20:16:29 -0400

    Create Remove_Leading_Trailing_Character.py

---
+++ Utilities/String/Remove_Leading_Trailing_Character.py
@@ -0,0 +1,3 @@
+str1 = ' Cysteine helps prevent side effects due to drug reactions and toxic chemicals '
+str1.strip()
+# 'Cysteine helps prevent side effects due to drug reactions and toxic chemicals'
