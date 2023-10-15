c4749b97493da2ee0b9f1ac4c6e0ce71089a6677
Anni <90419301+ScienceComputing@users.noreply.github.com>
2023-10-15 15:59:17 -0400

    Update Format_String.py

--- Utilities/String/Format_String.py
+++ Utilities/String/Format_String.py
@@ -6,6 +6,11 @@
 print('{} helps prevent side effects due to drug reactions and toxic chemicals'.format('Cystein'))
 # Cystein helps prevent side effects due to drug reactions and toxic chemicals
 
+str1 = '{} helps prevent side effects due to drug reactions and toxic chemicals'
+amino_acid = 'Cystein'
+str1.format(amino_acid)
+# 'Cystein helps prevent side effects due to drug reactions and toxic chemicals'
+
 # Pair up the girl and boy names: pairs
 pairs = zip(gene_names, protein_names)
 
