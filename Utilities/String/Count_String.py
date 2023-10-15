365fff605fb733b682f5075e5116e0658ac927f6
Anni <90419301+ScienceComputing@users.noreply.github.com>
2023-10-15 02:23:08 -0400

    Update Count_String.py

--- Utilities/String/Count_String.py
+++ Utilities/String/Count_String.py
@@ -4,3 +4,6 @@ str1.count('Cysteine')
 
 str1.count('Cysteine', 2, len(str1))
 # 1
+
+str1.replace('Cysteine ', '', 1)
+# 'Cysteine helps prevent side effects due to drug reactions and toxic chemicals'
