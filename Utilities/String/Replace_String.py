4bca25f16efb4667257a041a8ad8e69634dbf2cb
Anni <90419301+ScienceComputing@users.noreply.github.com>
2023-10-15 02:37:46 -0400

    Create Replace_String.py

---
+++ Utilities/String/Replace_String.py
@@ -0,0 +1,3 @@
+str1 = 'Cysteine Cysteine helps prevent side effects due to drug reactions and toxic chemicals'
+str1.replace('Cysteine ', '', 1)
+# 'Cysteine helps prevent side effects due to drug reactions and toxic chemicals'
