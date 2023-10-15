2f3ec6f379c2dfb87fe46c6e34d74ac03a8957a7
Anni <90419301+ScienceComputing@users.noreply.github.com>
2023-10-15 17:36:29 -0400

    Update Format_DateTime.py

--- Utilities/String/Format_DateTime.py
+++ Utilities/String/Format_DateTime.py
@@ -4,3 +4,9 @@ print(datetime.now())
 
 print("Today's date is {:%Y-%m-%d %H:%M}".format(datetime.now()))
 # Today's date is 2023-10-15 17:30
+
+from datetime import datetime
+get_date = datetime.now()
+message = "Good evening. Today is {today:%B %d, %Y}. It's {today:%H:%M} ... time to work!"
+print(message.format(today=get_date))
+# Good evening. Today is October 15, 2023. It's 17:36 ... time to work!
