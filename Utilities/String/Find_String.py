4008e35b2fa4962fad28d1f2ec7c0354d019829f
Anni <90419301+ScienceComputing@users.noreply.github.com>
2023-10-15 02:14:45 -0400

    Update Find_String.py

--- Utilities/String/Find_String.py
+++ Utilities/String/Find_String.py
@@ -1,4 +1,6 @@
 str1 = 'Cysteine helps prevent side effects due to drug reactions and toxic chemicals'
+
+# Method 1: .find()
 str1.find('Cysteine') # The find method returns the lowest index in the string where it can find the substring
 # 0
 
@@ -7,3 +9,10 @@ str1.find('Alanine')
 
 str1.find('Cysteine', 1, 9)
 # -1
+
+# Method 2: .index()
+str1.index('Cysteine')
+# 0
+
+str1.index('Alanine')
+# ValueError: substring not found
