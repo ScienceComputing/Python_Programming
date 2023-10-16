b20f50ac8c3582f527f5bd917a98d50506eb9c96
Anni <90419301+ScienceComputing@users.noreply.github.com>
2023-10-16 07:23:26 -0400

    Update Formatted_String_Literal.py

--- Utilities/String/Formatted_String_Literal.py
+++ Utilities/String/Formatted_String_Literal.py
@@ -1,6 +1,6 @@
 # f-strings: f'literal string{expression}'
 
-
+# Example:
 # Pair up the gene and protein names: pairs
 pairs = zip(gene_names, protein_names)
 # Iterate over pairs
@@ -9,4 +9,10 @@ for rank, pair in enumerate(pairs):
     gene_names, protein_names = pair
     # Print the rank and names associated with each rank
     print(f'Rank {rank+1}: gene_name} and {protein_name}') # {rank+1}, {gene_name}, {protein_name} 
+
+# Type conversions
+# !s: convert to a string
+# !r: convert to a printable string with quotes
+# !a: convert to a printable string with quotes, but escape the non-ASCII character
+
 
