3649b5ef82bef98634b97cc13ad0bc227e33e45a
Anni <90419301+ScienceComputing@users.noreply.github.com>
2023-10-16 21:57:43 -0400

    Update 3_Template_String.py

--- Utilities/String/3_Template_String.py
+++ Utilities/String/3_Template_String.py
@@ -2,3 +2,12 @@
 # Slower than f-strings; 
 # Don't allow format specifiers (e for scientific notation; f for float; d for digit); 
 # Good when working with externally formatted strings
+
+from string import Template
+str1 = Template('$amino_acid helps prevent side effects due to drug reactions and toxic chemicals.')
+str1.substitute(amino_acid='Cysteine')
+# 'Cysteine helps prevent side effects due to drug reactions and toxic chemicals.'
+
+Cys = 'Cysteine'
+str1.substitute(amino_acid=Cys)
+# 'Cysteine helps prevent side effects due to drug reactions and toxic chemicals.'
