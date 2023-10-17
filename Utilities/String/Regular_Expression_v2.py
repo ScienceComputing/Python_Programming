db2993fbc0819506a7fb70c14aab677d480e7277
Anni <90419301+ScienceComputing@users.noreply.github.com>
2023-10-17 13:21:11 -0400

    Update Regular_Expression_v2.py

--- Utilities/String/Regular_Expression_v2.py
+++ Utilities/String/Regular_Expression_v2.py
@@ -2,7 +2,7 @@
 # {3} {3, 4} -> Regular_Expresson_v1.py
 # +: once or more
 # *: zero times or more
-# ?: zero times or once
+# ?: zero times or once -> useful for detecting the word variation
 
 import re
 str1 = '56-9000998 WMM OP 9-0'
@@ -12,3 +12,7 @@ re.findall(r'\d+-\d+', str1)
 str2 = 'The visual abstract was amazing! @Limma&!n @John!!n @Jake09'
 re.findall(r'@\w+\W*\w+', str2)
 # ['@Limma&!n', '@John!!n', '@Jake09']
+
+str3 = 'The color of this image is wonderful! But, the colour could be more colorful.'
+re.findall(r'colou?r', str3)
+# ['color', 'colour', 'color']
