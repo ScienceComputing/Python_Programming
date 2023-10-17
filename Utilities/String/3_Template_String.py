8b85045834f4fc21cb062b66b9fb87d7fd0a1316
Anni <90419301+ScienceComputing@users.noreply.github.com>
2023-10-17 06:44:23 -0400

    Update 3_Template_String.py

--- Utilities/String/3_Template_String.py
+++ Utilities/String/3_Template_String.py
@@ -20,3 +20,18 @@ str2.substitute(function='drug reaction')
 str3 = Template('I paid for the Roma tomatoes only $$$price, amazing!') # $price -> identifier
 str3.substitute(price='1.5')
 # 'I paid for the Roma tomatoes only $1.5, amazing!'
+
+favor_donut = dict(flavor='Glazed donut')
+str4 = Template('I enjoy $flavor $donut very much!')
+try:
+    str4.substitute(favor_donut)
+except KeyError:
+    print('Missing information')
+
+# Missing information
+
+# Set up safe substitution whereby missing placeholders will appear in resulting string
+favor_donut = dict(flavor='Glazed donut')
+str4 = Template('I enjoy $flavor $donut very much!')
+str4.safe_substitute(favor_donut)
+# 'I enjoy Glazed donut $donut very much!'
