6a2cabf22a7ce3d2849d594d7443a4adbe3824b5
Anni <90419301+ScienceComputing@users.noreply.github.com>
2023-10-17 06:59:40 -0400

    Create Regular_Expression.py

---
+++ Utilities/String/Regular_Expression.py
@@ -0,0 +1,20 @@
+# Why use pattern matching?
+# 1. Find and replace text
+# 2. Validate strings
+
+# Example to construct a pattern:
+# r'st\d\s\w{3,10}'
+# r: raw string
+# st: these are normal characters; they exactly match s and t
+# ---The followings are the metacharacters
+# \d: a digit
+# \s: a whitespace
+# \w: a word character
+# {3, 10}: \w should appear between 3 and 10 times
+
+import re
+
+# Find all matches of a pattern
+# re.findall(r'regex', string)
+re.findall(r'Lessions in Chemistry', 'Love Lessions in Chemistry! Really love Lessions in Chemistry!')
+# ['Lessions in Chemistry', 'Lessions in Chemistry']
