a3792795a83202f3ca6e8f2f654a4c3ded84cad0
Anni <90419301+ScienceComputing@users.noreply.github.com>
2023-10-17 13:19:00 -0400

    Create Regular_Expression_v2.py

---
+++ Utilities/String/Regular_Expression_v2.py
@@ -0,0 +1,14 @@
+# Quantifier
+# {3} {3, 4} -> Regular_Expresson_v1.py
+# +: once or more
+# *: zero times or more
+# ?: zero times or once
+
+import re
+str1 = '56-9000998 WMM OP 9-0'
+re.findall(r'\d+-\d+', str1)
+# ['56-9000998', '9-0']
+
+str2 = 'The visual abstract was amazing! @Limma&!n @John!!n @Jake09'
+re.findall(r'@\w+\W*\w+', str2)
+# ['@Limma&!n', '@John!!n', '@Jake09']
