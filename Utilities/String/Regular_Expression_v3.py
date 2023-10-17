be840a18246e50c45d6809ef556958174a0fe7ee
Anni <90419301+ScienceComputing@users.noreply.github.com>
2023-10-17 14:06:30 -0400

    Update Regular_Expression_v3.py

--- Utilities/String/Regular_Expression_v3.py
+++ Utilities/String/Regular_Expression_v3.py
@@ -1,1 +1,11 @@
 # Topic: Regex metacharacters
+# re.match -> anchor at the begining of a string
+# re.search
+
+import re
+str1 = 'There are 8900023 students attending the NYU graduate school fair!'
+re.search(r'\d+', str1)
+# <re.Match object; span=(10, 17), match='8900023'>
+
+re.match(r'\d+', str1)
+# Nothing returns
