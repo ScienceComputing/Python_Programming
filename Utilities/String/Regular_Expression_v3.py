f9e52c5da5758ee82c33984d7313ea80f1b851de
Anni <90419301+ScienceComputing@users.noreply.github.com>
2023-10-17 14:09:12 -0400

    Update Regular_Expression_v3.py

--- Utilities/String/Regular_Expression_v3.py
+++ Utilities/String/Regular_Expression_v3.py
@@ -1,6 +1,7 @@
 # Topic: Regex metacharacters
 # re.match -> anchor at the begining of a string
 # re.search
+# special characters: . -> match any character except newline
 
 import re
 str1 = 'There are 8900023 students attending the NYU graduate school fair!'
@@ -9,3 +10,7 @@ re.search(r'\d+', str1)
 
 re.match(r'\d+', str1)
 # Nothing returns
+
+str2 = 'https://www.harvard.edu'
+re.findall(r'www.+edu', str2)
+# ['www.harvard.edu']
