b39217415b622c8b9d599989acfde48bbd1cf9e3
Anni <90419301+ScienceComputing@users.noreply.github.com>
2023-10-17 13:33:48 -0400

    Update Regular_Expression_v2.py

--- Utilities/String/Regular_Expression_v2.py
+++ Utilities/String/Regular_Expression_v2.py
@@ -17,6 +17,9 @@ str3 = 'The color of this image is wonderful! But, the colour could be more colo
 re.findall(r'colou?r', str3)
 # ['color', 'colour', 'color']
 
-# A regex to match a valid email/password address
+# A regex to match a valid link/date/token/email/password address
+link_regex = r'http\S+' # Very useful to use when you know a pattern doesn't contain spaces and you have reached the end when you do find one
+date_regex = r'\d{1,2}\w+\s\w+\s\d{4}\s\d{1,2}:\d{2}' # 1st October 2023 17:25
+token_regex = r'#\w+'
 email_regex = r'[A-Za-z0-9!#%&*$.]+@\w+\.com'
 password_regex = r'[a-zA-Z0-9*#$%!&.]{8,20}'
