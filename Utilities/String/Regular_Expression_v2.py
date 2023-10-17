7e4b829fee9cc51d68f5c23400f6d238db90e077
Anni <90419301+ScienceComputing@users.noreply.github.com>
2023-10-17 13:26:36 -0400

    Update Regular_Expression_v2.py

--- Utilities/String/Regular_Expression_v2.py
+++ Utilities/String/Regular_Expression_v2.py
@@ -1,6 +1,6 @@
-# Quantifier
-# {3} {3, 4} -> Regular_Expresson_v1.py
-# +: once or more
+# Quantifier - immediately to the left
+# {3} {3, 4} {3,} -> 3 times; 3 times at least, 4 times at most; 3 times at least
+# +: once or more -> r'violin+': + applies to n, not to violin 
 # *: zero times or more
 # ?: zero times or once -> useful for detecting the word variation
 
@@ -16,3 +16,7 @@ re.findall(r'@\w+\W*\w+', str2)
 str3 = 'The color of this image is wonderful! But, the colour could be more colorful.'
 re.findall(r'colou?r', str3)
 # ['color', 'colour', 'color']
+
+# A regex to match a valid email/password address
+email_regex = r'[A-Za-z0-9!#%&*$.]+@\w+\.com'
+password_regex = r'[a-zA-Z0-9*#$%!&.]{8,20}'
