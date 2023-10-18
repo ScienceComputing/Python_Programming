91218872698a4964ddc7c78dcff4ecb410b88dc1
Anni <90419301+ScienceComputing@users.noreply.github.com>
2023-10-18 16:04:28 -0400

    Update Common_Regex.py

--- Utilities/String/Common_Regex.py
+++ Utilities/String/Common_Regex.py
@@ -4,3 +4,4 @@ date_regex = r'\d{1,2}\w+\s\w+\s\d{4}\s\d{1,2}:\d{2}' # 1st October 2023 17:25
 token_regex = r'#\w+'
 email_regex = r'[A-Za-z0-9!#%&*$.]+@\w+\.com'
 password_regex = r'[a-zA-Z0-9*#$%!&.]{8,20}'
+html_tag_regex = r'<.+?>'
