cf204349903861534834aa552483bbae85ec3d91
Anni <90419301+ScienceComputing@users.noreply.github.com>
2023-10-18 15:46:58 -0400

    Update Regex_Quantifier.py

--- Utilities/String/Regex_Quantifier.py
+++ Utilities/String/Regex_Quantifier.py
@@ -1,13 +1,14 @@
 # Quantifier - immediately to the left
+# The following quantifiers are greedy; they tend to match as many characters as possible and return the longest match
 # {3} {3, 4} {3,} -> 3 times; 3 times at least, 4 times at most; 3 times at least
 # +: once or more -> r'violin+': + applies to n, not to violin 
 # *: zero times or more
 # ?: zero times or once -> useful for detecting the word variation
 
 import re
-str1 = '56-9000998 WMM OP 9-0'
+str1 = '1681 Broadway WMM OP 9-0'
 re.findall(r'\d+-\d+', str1)
-# ['56-9000998', '9-0']
+# ['9-0']
 
 str2 = 'The visual abstract was amazing! @Limma&!n @John!!n @Jake09'
 re.findall(r'@\w+\W*\w+', str2)
