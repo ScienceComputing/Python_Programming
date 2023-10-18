6905cd411ea8a7518adab23c46a039bc0cdd8d61
Anni <90419301+ScienceComputing@users.noreply.github.com>
2023-10-18 16:34:41 -0400

    Update and rename Match_Subpattern_Group.py to Find_Subpattern_Group.py

--- Utilities/String/Find_Subpattern_Group.py
+++ Utilities/String/Find_Subpattern_Group.py
@@ -8,3 +8,11 @@ re.findall(r'([A-Za-z]+)\s\w+\s(\d+)\s(\w+)', str1)
 a = re.findall(r'([A-Za-z]+)\s\w+\s(\d+)\s(\w+)', str1)
 a[0][1]
 # '3'
+
+# Apply the quantifier to the entire group
+str2 = '8F0j3r'
+re.search(r'(\d[A-Za-z])+', str2)
+# <re.Match object; span=(0, 6), match='8F0j3r'>
+
+re.search(r'\d[A-Za-z]+', str2)
+# <re.Match object; span=(0, 2), match='8F'>
