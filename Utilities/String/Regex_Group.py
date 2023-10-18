8be289a3e6ad97ca387fb9aa71387eb63b6beb9f
Anni <90419301+ScienceComputing@users.noreply.github.com>
2023-10-18 16:28:07 -0400

    Create Regex_Group.py

---
+++ Utilities/String/Regex_Group.py
@@ -0,0 +1,7 @@
+# Use () to group and capture characters together
+# ([A-Za-z]+)\s\w+\s(\d+)\s(\w+)
+
+import re
+str1 = 'Joty has 3 friends who she spends some time with every week. Zoey has 0 brothers.'
+re.findall(r'([A-Za-z]+)\s\w+\s(\d+)\s(\w+)', str1)
+# [('Joty', '3', 'friends'), ('Zoey', '0', 'brothers')]
