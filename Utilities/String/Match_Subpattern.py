5b2126afe57586a1add3ce809b900efd64c1a4ff
Anni <90419301+ScienceComputing@users.noreply.github.com>
2023-10-18 16:30:12 -0400

    Update and rename Regex_Group.py to Match_Subpattern.py

--- Utilities/String/Match_Subpattern.py
+++ Utilities/String/Match_Subpattern.py
@@ -5,3 +5,6 @@ import re
 str1 = 'Joty has 3 friends who she spends some time with every week. Zoey has 0 brothers.'
 re.findall(r'([A-Za-z]+)\s\w+\s(\d+)\s(\w+)', str1)
 # [('Joty', '3', 'friends'), ('Zoey', '0', 'brothers')]
+a = re.findall(r'([A-Za-z]+)\s\w+\s(\d+)\s(\w+)', str1)
+a[0][1]
+# '3'
