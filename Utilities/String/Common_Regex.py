a49895dc6178c1d14870b225113923d2f9d136be
Anni <90419301+ScienceComputing@users.noreply.github.com>
2023-10-18 16:54:13 -0400

    Update Common_Regex.py

--- Utilities/String/Common_Regex.py
+++ Utilities/String/Common_Regex.py
@@ -3,5 +3,6 @@ link_regex = r'http\S+' # Very useful to use when you know a pattern doesn't con
 date_regex = r'\d{1,2}\w+\s\w+\s\d{4}\s\d{1,2}:\d{2}' # 1st October 2023 17:25
 token_regex = r'#\w+'
 email_regex = r'[A-Za-z0-9!#%&*$.]+@\w+\.com'
+email_user_account = r'([A-Za-z0-9]+)@\S+' # e.g., oppert009@hotmail.com -> oppert009
 password_regex = r'[a-zA-Z0-9*#$%!&.]{8,20}'
 html_tag_regex = r'<.+?>'
