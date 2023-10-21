dc55d994bbf767f061c04f6dfc4f17e98d56665d
Anni <90419301+ScienceComputing@users.noreply.github.com>
2023-10-21 10:42:04 -0400

    Create Backreference.py

---
+++ Utilities/String/Backreference.py
@@ -0,0 +1,13 @@
+import re
+str1 = 'The Dr. Zkulla album is released on 2023-09-10.'
+info = re.search(r'(\d{4})-(\d{2})-(\d{2})', str1)
+# info
+# <re.Match object; span=(36, 46), match='2023-09-10'>
+info.group(0)
+# '2023-09-10'
+info.group(1)
+# '2023'
+info.group(2)
+# '09'
+info.group(3)
+# '10'
