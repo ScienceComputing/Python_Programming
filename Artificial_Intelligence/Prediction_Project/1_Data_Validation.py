81f65cba42692622c93543a65446a70fc7b0d8d2
Anni <90419301+ScienceComputing@users.noreply.github.com>
2023-10-25 11:29:06 -0700

    Update and rename Data_Validation.py to 1_Data_Validation.py

--- Artificial_Intelligence/Prediction_Project/1_Data_Validation.py
+++ Artificial_Intelligence/Prediction_Project/1_Data_Validation.py
@@ -13,7 +13,25 @@ plt.style.use('ggplot')
 
 df = pd.read_csv('data/car_data.csv')
 df.info()
+<class 'pandas.core.frame.DataFrame'>
+RangeIndex: 6738 entries, 0 to 6737
+Data columns (total 9 columns):
+ #   Column        Non-Null Count  Dtype  
+---  ------        --------------  -----  
+ 0   model         6738 non-null   object 
+ 1   year          6738 non-null   int64  
+ 2   price         6738 non-null   int64  
+ 3   transmission  6738 non-null   object 
+ 4   mileage       6738 non-null   int64  
+ 5   fuelType      6738 non-null   object 
+ 6   tax           6738 non-null   int64  
+ 7   mpg           6738 non-null   float64
+ 8   engineSize    6738 non-null   float64
+dtypes: float64(2), int64(4), object(3)
+memory usage: 473.9+ KB
+
 df.head()
+
 
 # Validate the variables
 # Count the number of unique values in the selected column
