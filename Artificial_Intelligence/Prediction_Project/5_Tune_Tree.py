15c5224d9f6a1350a200b90f5db6897c7e6fa1a4
Anni <90419301+ScienceComputing@users.noreply.github.com>
2023-10-25 13:58:54 -0700

    Update 5_Tune_Tree.py

--- Artificial_Intelligence/Prediction_Project/5_Tune_Tree.py
+++ Artificial_Intelligence/Prediction_Project/5_Tune_Tree.py
@@ -1,1 +1,20 @@
-# Tune the parameter for Decision Tree Regression Model
+# Tune the maximum depth of the tree for Decision Tree Regression Model
+train_score = []
+test_score = []
+max_score = 0
+max_pair = (0,0)
+
+for i in range(1,50):
+    tree = DecisionTreeRegressor(max_depth=i,random_state=99)
+    tree.fit(X_train,y_train)
+    y_pred = tree.predict(X_test)
+    train_score.append(tree.score(X_train,y_train))
+    test_score.append(r2_score(y_test,y_pred))
+    test_pair = (i,r2_score(y_test,y_pred))
+    if test_pair[1] > max_pair[1]:
+        max_pair = test_pair
+
+fig, ax = plt.subplots()
+ax.plot(np.arange(1,50), train_score, label = "Training R^2",color='lightcoral')
+ax.plot(np.arange(1,50), test_score, label = "Testing R^2",color='lime')
+print(f'Best max_depth is: {max_pair[0]} \nTesting R^2 is: {max_pair[1]}')
