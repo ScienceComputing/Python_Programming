c591d06d80bc54b2c48321639e56f5af09b229ab
Anni <90419301+ScienceComputing@users.noreply.github.com>
2023-10-25 13:08:02 -0700

    Update 4_Build_Decision_Tree_Regression.py

--- Artificial_Intelligence/Prediction_Project/4_Build_Decision_Tree_Regression.py
+++ Artificial_Intelligence/Prediction_Project/4_Build_Decision_Tree_Regression.py
@@ -13,6 +13,14 @@ from sklearn.metrics import r2_score,mean_squared_error
 plt.style.use('ggplot')
 
 # Build the tree model
-tree = DecisionTreeRegressor(max_depth=12,min_samples_split=2,random_state=42)
+tree = DecisionTreeRegressor(max_depth=12,min_samples_split=2,random_state=99)
 tree.fit(X_train,y_train)
-y_pred2 = tree.predict(X_test)
+y_pred_2 = tree.predict(X_test)
+
+# Use R-squared and RMSE to evaulate the model performance
+d_r2 = tree.score(X_test, y_test)
+print("Decision Tree Regressor R-squared: {}".format(d_r2))
+
+d_mse = mean_squared_error(np.exp(y_pred_2), np.exp(y_test))
+d_rmse = np.sqrt(d_mse)
+print("Decision Tree Regressor RMSE: {}".format(d_rmse))
