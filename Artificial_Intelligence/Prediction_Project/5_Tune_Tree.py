47e782e90ac2646918667a5b2fc58e868b93e45d
Anni <90419301+ScienceComputing@users.noreply.github.com>
2023-10-25 14:00:21 -0700

    Update 5_Tune_Tree.py

--- Artificial_Intelligence/Prediction_Project/5_Tune_Tree.py
+++ Artificial_Intelligence/Prediction_Project/5_Tune_Tree.py
@@ -1,3 +1,17 @@
+# Set up the environment
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.style as style
+import seaborn as sns
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
+from sklearn.tree import DecisionTreeRegressor
+from sklearn.preprocessing import LabelEncoder
+from sklearn.preprocessing import PowerTransformer
+from sklearn.metrics import r2_score,mean_squared_error
+plt.style.use('ggplot')
+
 # Tune the maximum depth of the tree for Decision Tree Regression Model
 train_score = []
 test_score = []
