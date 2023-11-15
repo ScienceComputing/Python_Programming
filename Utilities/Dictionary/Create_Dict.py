8953cacf6788b6dcbf6dc99b0656fd34ceddcb41
Anni <90419301+ScienceComputing@users.noreply.github.com>
2023-11-14 19:35:21 -0500

    Update Create_Dict.py

--- Utilities/Dictionary/Create_Dict.py
+++ Utilities/Dictionary/Create_Dict.py
@@ -4,4 +4,11 @@ my_yogurt = {
   'filmjolk': 4.6,
   'regular yogurt': 3.7
 }
+print(my_yogurt['filmjolk'])
+print(my_yogurt.get['filmjolk2']) # None
+
+# Obtain items
+print(my_yogurt.items())
+print(my_yogurt.keys())
+print(my_yogurt.values())
   
