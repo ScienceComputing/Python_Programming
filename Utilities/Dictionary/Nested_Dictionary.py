1ebfad503fa51adf07a26458595ee8d6d83b4661
Anni <90419301+ScienceComputing@users.noreply.github.com>
2023-11-14 19:44:29 -0500

    Update Nested_Dictionary.py

--- Utilities/Dictionary/Nested_Dictionary.py
+++ Utilities/Dictionary/Nested_Dictionary.py
@@ -1,6 +1,19 @@
 # Access the elements in a nested dictionary
 # Method 1:
-amino_acid_galleries = {'Cysteine': {'Formula': 'HOOC−CH(−NH2)−CH2−SH', 'Function': 'Help prevent side effects due to drug reactions and toxic chemicals'}}
+amino_acid_galleries = {
+    'Cysteine': {
+         'Formula': 'HOOC−CH(−NH2)−CH2−SH', 
+         'Function': 'Help prevent side effects due to drug reactions and toxic chemicals'
+    },
+    'Serine': {
+        'Formula': 'HO2C−CH(NH2)−CH2OH',
+        'Function': 'Involved in the synthesis of purines and pyrimidines'
+    },
+    'Lysine': {
+        'Formula': 'H2N−(CH2)4−CH(NH2)−COOH',
+        'Function': 'Used in the biosynthesis of proteins'
+    }
+}
 amino_acid_galleries['Cysteine']['Formula']
 # 'HOOC−CH(−NH2)−CH2−SH'
 
