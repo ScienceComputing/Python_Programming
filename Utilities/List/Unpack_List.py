9d6e2ed4c913e33dd0c4d7d6e2356e41e76750d7
Anni <90419301+ScienceComputing@users.noreply.github.com>
2023-11-19 20:30:41 -0500

    Update Unpack_List.py

--- Utilities/List/Unpack_List.py
+++ Utilities/List/Unpack_List.py
@@ -1,4 +1,4 @@
-# Unpacking lists
+# Unpack lists
 data = [ 'XYZ', 90, 71.1, (2023, 12, 21) ] 
 _, shares, price, _ = data
 shares
@@ -14,3 +14,9 @@ year
 line = 'https://ftp.ncbi.nlm.nih.gov/geo/series/GSE85nnn/GSE85241/suppl/GSE85241%5Fcellsystems%5Fdataset%5F4donors%5Fupdated%2Ecsv%2Egz'
 ncbi, base_name = line.split('suppl/')
 base_name
+
+df = pd.DataFrame(columns=[*adata_cell_pop.var_names, *obs_to_keep])
+
+# The * operator is used to unpack elements from the lists (or any iterable) adata_cell_pop.var_names and obs_to_keep. 
+# This operator is often used to unpack iterables in Python.
+# For example, if adata_cell_pop.var_names is ['gene1', 'gene2', 'gene3'] and obs_to_keep is ['age', 'gender'], then [*adata_cell_pop.var_names, *obs_to_keep] becomes ['gene1', 'gene2', 'gene3', 'age', 'gender'].
