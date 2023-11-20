f994e56e95cd5ec313301eceb47a6e93483c1771
Anni <90419301+ScienceComputing@users.noreply.github.com>
2023-11-19 20:50:34 -0500

    Update Dictionary_Comprehension.py

--- Utilities/Dictionary/Dictionary_Comprehension.py
+++ Utilities/Dictionary/Dictionary_Comprehension.py
@@ -2,3 +2,6 @@
 square = {num: num ** 2 for num in range(9)}
 print(square)
 # {0: 0, 1: 1, 2: 4, 3: 9, 4: 16, 5: 25, 6: 36, 7: 49, 8: 64}
+
+import anndata as ad
+sum_dict = {gene: 'sum' for gene in adata_replicate.var_names}
