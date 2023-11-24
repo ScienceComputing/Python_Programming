9700b23db0f1a24d34c66b81b360d150983f17e6
Anni <90419301+ScienceComputing@users.noreply.github.com>
2023-11-24 08:07:22 -0500

    Create Empty_Class.py

---
+++ Utilities/Class/Empty_Class.py
@@ -0,0 +1,9 @@
+class Music:
+  pass
+
+m1 = Music()
+m1 # <__main__.Music object at 0x1013d3fd0>
+m2 = Music()
+m2 # <__main__.Music object at 0x1013e80d0>
+# object Music is defined within the main module. 
+# The string (" object at 0x1013d3fd0") is a memory address, indicating the location of the Music object in memory.
