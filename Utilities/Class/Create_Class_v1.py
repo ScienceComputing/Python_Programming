2e9e1341441364789e2338796725a2f7c386b7e2
Anni <90419301+ScienceComputing@users.noreply.github.com>
2023-11-24 11:00:48 -0500

    Update Create_Class_v1.py

--- Utilities/Class/Create_Class_v1.py
+++ Utilities/Class/Create_Class_v1.py
@@ -6,7 +6,7 @@ class Falafel:
         if self.type not in ['fried','baked']:
             print('Unrecognized type: ' + str(self.type))
             print('Type will be set to baked')
-            self.type ='baked'
+            self.type = 'baked'
         self.origin = 'Egyptian cuisine'
     def eat(self):
         print('I got eaten')
