a28404b91f06184382a7396b3867b50cd3f7039f
Anni <90419301+ScienceComputing@users.noreply.github.com>
2023-11-24 08:12:09 -0500

    Update Create_Class_v1.py

--- Utilities/Class/Create_Class_v1.py
+++ Utilities/Class/Create_Class_v1.py
@@ -1,5 +1,6 @@
+# Add methods/functions to a class
 class Falafel:
-    def __init__(self,type='baked'):
+    def __init__(self,type='baked'): # use self as the 1st argument in method definition; self refers to the data of a particualr object
         self.ingrediate = ['Chickpeas', 'Onion', 'Parsley', 'Garlic', 'Green Chile Pepper', 'Oil', 'Baking Soda']
         self.type = type
         if self.type not in ['fried','baked']:
@@ -27,7 +28,7 @@ class MultiFalafel(Falafel):
         self.remaining = self.number - self.number_eat
 
 # Test the code
-# six_falafels = MultiFalafel(num=6,num_to_eat=7)
+# six_falafels = MultiFalafel(num=6,num_to_eat=7) # Ignore self when calling method on an object
 # six_falafels.eat()
 # six_falafels.remaining
 # six_falafels.type
