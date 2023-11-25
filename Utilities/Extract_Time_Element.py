5de73dfec5a148fce4c9d84dff461b9d54381f3d
Anni <90419301+ScienceComputing@users.noreply.github.com>
2023-11-25 11:46:12 -0500

    Update Extract_Time_Element.py

--- Utilities/Extract_Time_Element.py
+++ Utilities/Extract_Time_Element.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 class ExtractTimeElement:    
     def __init__(self, year, month, day):
       self.year, self.month, self.day = year, month, day
@@ -6,8 +7,17 @@ class ExtractTimeElement:
         element = date_str.split("-")
         year, month, day = int(element[0]), int(element[1]), int(element[2])
         return cls(year, month, day)
+    @classmethod
+    def from_datetime(cls, date_obj):
+        year, month, day = date_obj.year, date_obj.month, date_obj.day
+        return cls(year, month, day)
         
 target_time = ExtractTimeElement.from_str('2023-01-30')   
 print(target_time.year) # 2023
 print(target_time.month) # 01
 print(target_time.day) # 30
+
+target_time_2 = ExtractTimeElement.from_datetime(datetime.today())   
+print(target_time_2.year) 
+print(target_time_2.month)
+print(target_time_2.day)
