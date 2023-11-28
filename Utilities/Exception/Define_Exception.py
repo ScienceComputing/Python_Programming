29d548477aa164b2f287d1a622a1f1a194647eca
Anni <90419301+ScienceComputing@users.noreply.github.com>
2023-11-27 23:32:04 -0500

    Update Define_Exception.py

--- Utilities/Exception/Define_Exception.py
+++ Utilities/Exception/Define_Exception.py
@@ -1,3 +1,20 @@
-class DenominatorError(ValueError): pass
+class DivisionError(ValueError):
+    pass
 
-class FractionError(DenominatorError): pass
+class IntegerDivisionError(DivisionError):
+    pass
+
+class Divider:
+    def divide(self, a, b):
+        if b == 0:
+            raise DivisionError("Division by zero!")
+        if a % b != 0:
+            raise IntegerDivisionError("Result is not an integer!")
+        return a // b
+
+divider = Divider()
+result = divider.divide(10, 2)  
+result = divider.divide(10, 3)  
+# IntegerDivisionError: Result is not an integer!
+result = divider.divide(10, 0)  
+# DivisionError: Division by zero!
