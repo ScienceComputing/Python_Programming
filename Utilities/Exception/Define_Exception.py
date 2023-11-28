35cacc417a787d6307af6b06dc2200b8be443d70
Anni <90419301+ScienceComputing@users.noreply.github.com>
2023-11-27 23:36:29 -0500

    Update Define_Exception.py

--- Utilities/Exception/Define_Exception.py
+++ Utilities/Exception/Define_Exception.py
@@ -8,9 +8,9 @@ class Divider:
     def divide(self, a, b):
         if b == 0:
             raise DivisionError("Division by zero!")
-        if a % b != 0:
+        if a % b != 0: # Check if the remainder of the division of a by b is not equal to zero. 
             raise IntegerDivisionError("Result is not an integer!")
-        return a // b
+        return a // b # Floor division: return the largest integer that is less than or equal to the result.
 
 divider = Divider()
 result = divider.divide(10, 2)  
