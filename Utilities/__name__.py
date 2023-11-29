e3770ab564d2e44d1c8d10e1b94ec10fcb461672
Anni <90419301+ScienceComputing@users.noreply.github.com>
2023-11-29 12:25:13 -0500

    Update __name__.py

--- Utilities/__name__.py
+++ Utilities/__name__.py
@@ -1,11 +1,17 @@
-# moduleabc.py
+# It's a common practice to use this attribute `__name__` to allow or prevent parts of code from being run when the module is imported versus executed directly.
+
+# touch moduleabc.py
+# vim moduleabc.py
+# Inside the moduleabc.py, insert the following python code:
 def abc_function():
     print("This is a abc function")
 
 if __name__ == '__main__':
     print("This is the main part of the script")
 
-# another_script.py
+# Open the python from command line interface
+# __name__ is set to the name of the module. 
+# Hence, as our script is named moduleabc.py, __name__ will be 'moduleabc' when it is imported. 
 import moduleabc as mabc
-
 mabc.abc_function()
+# This is a abc function
