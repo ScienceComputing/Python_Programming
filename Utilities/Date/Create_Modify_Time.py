290821a7627f543471df3335af3f011b40e83527
Anni <90419301+ScienceComputing@users.noreply.github.com>
2023-11-29 17:19:50 -0500

    Create Create_Modify_Time.py

---
+++ Utilities/Date/Create_Modify_Time.py
@@ -0,0 +1,11 @@
+# import datetime class from the datetime package
+from datetime import datetime
+
+dt = datetime(year=2023, month=1, day=6, hour=14, minute=30, second=25, microsecond=500000)
+# 0.5 second = 500000 microseconds
+print(dt)
+# 2023-01-06 14:30:25.500000
+
+dt_2 = dt.replace(minute=18, second=9, microsecond=0)
+print(dt_2)
+# 2023-01-06 14:18:09
