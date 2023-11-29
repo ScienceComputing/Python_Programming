9fcd5f1dd11393e69c14cabab7a5e7a0ca472c1c
Anni <90419301+ScienceComputing@users.noreply.github.com>
2023-11-29 16:02:52 -0500

    Update Create_Date.py

--- Utilities/Date/Create_Date.py
+++ Utilities/Date/Create_Date.py
@@ -1,3 +1,8 @@
 from datetime import date
 birthdate_list = ['03/09/1998', '12/07/2000']
 
+birthdate_list_2 = [date(1998, 3, 9), date(2000, 12, 7)]
+print(birthdate_list_2[1].year) # 2000
+print(birthdate_list_2[1].month) # 12
+print(birthdate_list_2[1].day) # 7
+
