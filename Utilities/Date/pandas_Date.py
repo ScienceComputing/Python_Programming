07ed2a1cbd093d0de78e730e77533d5a68a372f6
Anni <90419301+ScienceComputing@users.noreply.github.com>
2023-11-30 15:44:30 -0500

    Update pandas_Date.py

--- Utilities/Date/pandas_Date.py
+++ Utilities/Date/pandas_Date.py
@@ -1,7 +1,21 @@
-# Parse the date columns
+"""
+Parse the date columns
+"""
 import pandas as pd
 
 eb = pd.read_csv('energy_bar_sales.csv', 
                  parse_dates = ['start date', 'end date'])
 
 print(eb.iloc[0]) # Retrieve the 0th row
+
+# Alternative approach
+eb['start date'] = pd.to_datetime(eb['start date'], format = '%Y-%m-%d %H:%M:%S')
+
+eb['duration'] = eb['end date'] - eb['start date']
+print(eb['duration'].head(3))
+
+"""
+Transform the duration into seconds
+"""
+eb['duration'].dt.total_seconds().head(3)
+# Access all of the typical datetime methods within the namespace `.dt`.
