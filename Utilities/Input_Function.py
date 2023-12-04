fd612a6367f7722983a314246c31457a6c9685ac
Anni <90419301+ScienceComputing@users.noreply.github.com>
2023-12-04 16:28:05 -0500

    Create Input_Function.py

---
+++ Utilities/Input_Function.py
@@ -0,0 +1,30 @@
+def math_2_num(x, y):
+    if y == 0:
+        raise ValueError("Division by zero is not allowed.")
+    addition = x + y
+    yield f"The addition of the two numbers is: {addition}"
+    subtract = x - y
+    yield f"The subtraction of the two numbers is: {subtract}"
+    multiply = x * y
+    yield f"The multiplication of the two numbers is: {multiply}"
+    division = x / y
+    yield f"The division of the two numbers is: {division}"
+    # Calculate the derivative of the functions with respect to x
+    derivative_addition = 1
+    yield f"The derivative of addition with respect to x is: {derivative_addition}"
+    derivative_subtract = 1
+    yield f"The derivative of subtraction with respect to x is: {derivative_subtract}"
+    derivative_multiply = y
+    yield f"The derivative of multiplication with respect to x is: {derivative_multiply}"
+    derivative_division = 1 / y
+    yield f"The derivative of division with respect to x is: {derivative_division}"
+
+# Ask the users to input the first number of their choice
+num1 = int(input("Please enter the first number: "))
+# Ask the users to input the second number of their choice
+num2 = int(input("Please enter the second number: "))
+
+print("The two numbers are", num1, "and", num2)
+
+for result in math_2_num((num1, num2):
+    print(result)
