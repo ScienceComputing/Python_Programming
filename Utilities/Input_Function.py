d3be2e10193615616e022118bc4544a3660105b0
Anni <90419301+ScienceComputing@users.noreply.github.com>
2023-12-04 16:33:49 -0500

    Update Input_Function.py

--- Utilities/Input_Function.py
+++ Utilities/Input_Function.py
@@ -28,3 +28,34 @@ print("The two numbers are", num1, "and", num2)
 
 for result in math_2_num((num1, num2):
     print(result)
+
+
+def math_2_num_2(x, y):
+    addition = x + y
+    yield f"The addition of the two numbers is: {addition}"
+    subtract = x - y
+    yield f"The subtraction of the two numbers is: {subtract}"
+    multiply = x * y
+    yield f"The multiplication of the two numbers is: {multiply}"
+    derivative_multiply = y
+    yield f"The derivative of multiplication with respect to x is: {derivative_multiply}"
+    if y != 0:
+        division = x / y
+        yield f"The division of the two numbers is: {division}"
+        derivative_addition = 1
+        yield f"The derivative of addition with respect to x is: {derivative_addition}"
+        derivative_subtract = 1
+        yield f"The derivative of subtraction with respect to x is: {derivative_subtract}"
+        derivative_division = 1 / y
+        yield f"The derivative of division with respect to x is: {derivative_division}"
+    else:
+        yield "Division and its related derivative are undefined when the second number is zero."
+
+num1 = int(input("Please enter the first number: "))
+num2 = int(input("Please enter the second number: "))
+
+print("The two numbers are", num1, "and", num2)
+
+for result in math_2_num_2(num1, num2):
+    print(result)
+
