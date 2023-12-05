ea8e8376c8b011fc1ed30626fdbaf84b2000cf7d
Anni <90419301+ScienceComputing@users.noreply.github.com>
2023-12-05 07:46:40 -0500

    Update Counter.py

--- Utilities/List/Counter.py
+++ Utilities/List/Counter.py
@@ -28,6 +28,20 @@ def element_counter(obj, n_most_common=3):
   top_elements = counter_obj.most_common(n_most_common)
   return top_elements
 
+# Visualize the frequency of most common elements
+import matplotlib.pyplot as plt
+
+def plot_most_common_element(top_elements):
+    top_elements_dict = dict(top_elements)
+    plt.figure()
+    plt.bar(range(len(top_elements_dict)), list(top_elements_dict.values()), align='center')
+    plt.xticks(range(len(top_elements_dict)), list(top_elements_dict.keys()), rotation='vertical')
+    plt.tight_layout()
+    plt.show()
+
 list2 = ['a', 'a', 'b', 'c', 'a', 'a', 'd', 'c', 100]
 element_counter(list2)
 # [('a', 4), ('c', 2), ('b', 1)]
+plot_most_common_element(element_counter(list2))
+
+
