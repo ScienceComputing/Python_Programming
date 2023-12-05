13f1a6e2d43ed01c261eb0b220def063c2e73ea9
Anni <90419301+ScienceComputing@users.noreply.github.com>
2023-12-05 07:39:21 -0500

    Update Counter.py

--- Utilities/List/Counter.py
+++ Utilities/List/Counter.py
@@ -1,4 +1,8 @@
 from collections import Counter
+
+"""
+Case 1
+"""
 list1 = ['a', 'a', 'b']
 list1_freq = Counter(list1)
 print(list1_freq)
@@ -6,9 +10,24 @@ print(list1_freq)
 print(list1_freq['a'])
 # 2
 
+"""
+Case 2
+"""
 list2 = ['a', 'a', 'b', 'c', 'a', 'a', 'd', 'c', 100]
 list2_freq = Counter(list2)
 list2_freq.most_common()
 # [('a', 4), ('c', 2), ('b', 1), ('d', 1), (100, 1)]
 list2_freq.most_common(3)
+# [('a', 4), ('c', 2), ('b', 1)]
+
+"""
+Case 3
+"""
+def element_counter(obj, n_most_common=3):
+  counter_obj = Counter(obj)
+  top_elements = counter_obj.most_common(n_most_common)
+  return top_elements
+
+list2 = ['a', 'a', 'b', 'c', 'a', 'a', 'd', 'c', 100]
+element_counter(list2)
 # [('a', 4), ('c', 2), ('b', 1)]
