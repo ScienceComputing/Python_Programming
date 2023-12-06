ff461305982a4e7b731c9f88f402ff724f23e7af
Anni <90419301+ScienceComputing@users.noreply.github.com>
2023-12-06 07:22:40 -0500

    Update Internal_Attribute_Method.py

--- Utilities/Class/Internal_Attribute_Method.py
+++ Utilities/Class/Internal_Attribute_Method.py
@@ -1,21 +1,23 @@
 class IntronicVariant:
-    _MAX_POSITION = 1000000 # Internal attribute which does not belong to the public class interface, and can be subject to change without notice
+    _MAX_POSITION = 1000000 # Internal attribute which does not belong to the public class interface, and can be subject to change without notice.
     _MAX_ALLELE_LENGTH = 50
     def __init__(self, chrom, position, ref, alt):
         self.chrom, self.position, self.ref, self.alt = chrom, position, ref, alt
+        self.valid = self._is_valid() # Let the validation process occur instantly, without requiring any user deliberation.
     @classmethod
     def from_str(cls, variantstr):
         chrom, position, ref, alt = variantstr.split(":")
         return cls(chrom, int(position), ref, alt)
     # _is_valid(): check if the variant attributes are within valid ranges
-    # This is an internal method which does not belong to the public class interface, and can be subject to change without notice
+    # This is an internal method which does not belong to the public class interface, and can be subject to change without notice.
     def _is_valid(self):
-        return (0 < self._position <= IntronicVariant._MAX_POSITION) and \
-               (1 <= len(self._ref) <= IntronicVariant._MAX_ALLELE_LENGTH) and \
-               (1 <= len(self._alt) <= IntronicVariant._MAX_ALLELE_LENGTH)
+        return (0 < self.position <= IntronicVariant._MAX_POSITION) and \
+               (1 <= len(self.ref) <= IntronicVariant._MAX_ALLELE_LENGTH) and \
+               (1 <= len(self.alt) <= IntronicVariant._MAX_ALLELE_LENGTH)
 
 variant1 = IntronicVariant("chr1", 500000, "A", "T")
 print(variant1._is_valid()) # True
+print(variant1.valid) # True
 
 variant2 = IntronicVariant("chr2", 1500000, "CAG", "G")
 print(variant2._is_valid()) # False 
