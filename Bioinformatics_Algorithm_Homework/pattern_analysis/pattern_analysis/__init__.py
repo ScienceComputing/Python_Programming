ce1193c12ca4e1c686be3abb5c97a2338aebbc95
Anni <90419301+ScienceComputing@users.noreply.github.com>
2023-12-12 08:00:30 -0500

    Delete Bioinformatics_Algorithm_Homework/pattern_analysis directory

--- Bioinformatics_Algorithm_Homework/pattern_analysis/pattern_analysis/__init__.py
+++
@@ -1,5 +0,0 @@
-"""
-pattern_analysis
-========
-A package for extracting pattern insights from the biological data.
-"""
--- Bioinformatics_Algorithm_Homework/pattern_analysis/pyment.sh
+++
@@ -1,2 +0,0 @@
-# Run the followng command in the terminal under the parent directory of the package pattern_analysis
-pyment -w -o numpydoc pattern_analysis/pattern_analysis.py
