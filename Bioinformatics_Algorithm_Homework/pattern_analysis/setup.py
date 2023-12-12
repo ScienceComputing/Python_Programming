c711a67187f1b221a0bb557dc29cc8907485ae06
Anni <90419301+ScienceComputing@users.noreply.github.com>
2023-12-12 08:22:59 -0500

    Update setup.py

--- Bioinformatics_Algorithm_Homework/pattern_analysis/setup.py
+++ Bioinformatics_Algorithm_Homework/pattern_analysis/setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
     author="Anni Liu",
     description="A package for analyzing patterns for genome sequences",
-    name="impyrial",
+    name="pattern_analysis",
     packages=find_packages(include=["pattern_analysis", "pattern_analysis.*"]),
     version="0.1.0",
 )
