c6ad6d06dd1bd58f01f01aecc48a336ae1944c88
Anni <90419301+ScienceComputing@users.noreply.github.com>
2023-12-14 08:17:52 -0500

    Update pattern_analysis.py

--- Bioinformatics_Algorithm_Homework/pattern_analysis/pattern_analysis/pattern_analysis.py
+++ Bioinformatics_Algorithm_Homework/pattern_analysis/pattern_analysis/pattern_analysis.py
@@ -5,17 +5,38 @@ def pattern_count(Text, Pattern):
     ----------
     Text : str
         A genome sequence, e.g., ATTTTTCGTTTAAA.
-        
     Pattern : str
         A k-mier pattern of interest, e.g., TTTA
 
     Returns
     -------
     int
-
+    
     """
     count = 0
     for i in range(len(Text)-len(Pattern)+1):
         if Text[i:i+len(Pattern)] == Pattern:
             count = count+1
     return count 
+
+mapping = {
+    'A': 'T',
+    'T': 'A',
+    'G': 'C',
+    'C': 'G'
+}
+
+def pattern_complement(Pattern):
+    """Convert a DNA sequence pattern into its complement counterpart.
+
+    Parameters
+    ----------
+    Pattern : str
+        A DNA sequence pattern of interest, e.g., CCGTAGCCC.
+
+    Returns
+    -------
+    str
+
+    """
+    return ''.join(mapping[i] for i in Pattern)
