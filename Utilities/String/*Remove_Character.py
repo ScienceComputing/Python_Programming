e76b62c04186ac7bf45957643227f280e3d17ff7
Anni <90419301+ScienceComputing@users.noreply.github.com>
2023-12-15 23:21:28 -0500

    Update *Remove_Character.py

--- Utilities/String/*Remove_Character.py
+++ Utilities/String/*Remove_Character.py
@@ -2,7 +2,7 @@ str1 = ' Cysteine helps prevent side effects due to drug reactions and toxic che
 str1.strip()
 # 'Cysteine helps prevent side effects due to drug reactions and toxic chemicals'
 
-str1.rstrip() # Right remove
+str1.rstrip() # Right remove: remove any trailing characters at the end a string; space is the default trailing character to remove
 # ' Cysteine helps prevent side effects due to drug reactions and toxic chemicals'
 
 str1.lstrip() # Left remove
