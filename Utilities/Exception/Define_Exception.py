a97b7a9f7f4d0eca44f2d45765098d89e5da6746
Anni <90419301+ScienceComputing@users.noreply.github.com>
2023-12-17 09:08:38 -0500

    Update Define_Exception.py

--- Utilities/Exception/Define_Exception.py
+++ Utilities/Exception/Define_Exception.py
@@ -1,3 +1,6 @@
+"""
+Case 1
+"""
 class DivisionError(ValueError):
     pass
 
@@ -39,3 +42,41 @@ except DivisionError:
     print("DivisionError caught")
 
 # IntegerDivisionError caught
+
+"""
+Case 2
+"""
+import numpy as np
+
+def split_scRNAseq_data(data_matrix):
+    """
+    Split single-cell RNA-seq data into training and testing sets.
+
+    Args:
+        data_matrix (numpy.ndarray): The input data matrix with cells as rows and genes as columns.
+
+    Returns:
+        tuple: A tuple containing two numpy arrays, (training_data, testing_data).
+    """
+    num_cells, num_genes = data_matrix.shape
+    if num_cells < 2000:
+        raise ValueError("Input data_matrix must have at least 2000 cells, it actually has just {0}".format(num_cells))
+    num_training = int(0.7 * num_cells)
+    permuted_indices = np.random.permutation(num_cells)
+    return data_matrix[permuted_indices[:num_training], :], data_matrix[permuted_indices[num_training:], :]
+
+import pytest
+
+def test_split_scRNAseq_data():
+    # Test case with 2 cells
+    test_data = np.array([[13, 39, 90, 100, 29], [15, 6, 7, 12, 0]])
+    with pytest.raises(ValueError) as exc_info:
+        split_scRNAseq_data(test_data)
+    expected_error_msg = "Input data_matrix must have at least 2000 cells, it actually has just 2"
+    assert exc_info.match(expected_error_msg)
+
+# Run the test function
+test_split_scRNAseq_data() # Nothing is returned, suggesting this test on the exception passes.
+
+
+
