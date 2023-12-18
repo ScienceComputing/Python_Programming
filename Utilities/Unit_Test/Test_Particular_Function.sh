c2db370fb272c3a0291cdc2516b4847d0d0b6f01
Anni <90419301+ScienceComputing@users.noreply.github.com>
2023-12-18 09:47:59 -0500

    Create Test_Particular_Function.sh

---
+++ Utilities/Unit_Test/Test_Particular_Function.sh
@@ -0,0 +1,6 @@
+# Terminal
+pytest -k "complement"
+# ======================= 1 passed, 1 deselected in 0.01s ========================
+
+pytest -k "test_pattern"
+# ============================== 2 passed in 0.01s ===============================
