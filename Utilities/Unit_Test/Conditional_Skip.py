f3d8448483b45a6230941ca873d340dda3ecce8c
Anni <90419301+ScienceComputing@users.noreply.github.com>
2023-12-19 07:48:51 -0500

    Update Conditional_Skip.py

--- Utilities/Unit_Test/Conditional_Skip.py
+++ Utilities/Unit_Test/Conditional_Skip.py
@@ -2,7 +2,7 @@ import sys
 import pytest
 import numpy as np
 
-class TestGetDataAsNumpyArray(object):
+class TestReturnDataAsNumpyArray(object):
     @pytest.mark.skipif(
         sys.version_info > (2, 7) or sys.platform != "linux",
         reason="Skipped on Python 2.7 or higher and non-Linux platforms"
@@ -14,9 +14,9 @@ class TestGetDataAsNumpyArray(object):
                              [666.2, 123.5]
                              ]
                             )
-        result = get_data_as_numpy_array("raw_data.txt", num_columns=2)
-        message = "Expected return value: {0}, Actual return value: {1}".format(expected, result)
-        assert result == pytest.approx(expected), message
+        result = return_data_as_numpy_array("raw_data.txt", num_columns=2)
+        error_message = "Expected return value: {0}, Actual return value: {1}".format(expected, result)
+        assert result == pytest.approx(expected), error_message
 
 # Terminal:
 # pytest -rs
