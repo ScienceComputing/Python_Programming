907863077bdd121bd192d868f4c8c5426c3128c8
Anni <90419301+ScienceComputing@users.noreply.github.com>
2023-12-19 07:56:52 -0500

    Create Install_Editable_Package.sh

---
+++ Utilities/Install_Editable_Package.sh
@@ -0,0 +1,4 @@
+pip install -e .
+# Install the project as a package in editable mode. 
+# Any changes we make to the source code within that project directory will be immediately reflected in the installed package.
+# It is commonly used during development when we are actively working on the code and want to test changes without reinstalling the package after each modification.
