a6c295092d75874129fe154283befa7d8876741c
Anni <90419301+ScienceComputing@users.noreply.github.com>
2023-12-24 18:16:00 -0500

    Update Replace_Value.py

--- Utilities/pandas/Replace_Value.py
+++ Utilities/pandas/Replace_Value.py
@@ -5,3 +5,6 @@ df['col_name'].replace('ori_val', 'new_val', inplace=True)
 df['col_name'].replace(['ori_val_1', 'ori_val_2'], 'new_val', inplace=True)
 df['col_name'].replace(['ori_val_1', 'ori_val_2'], ['new_val_1', 'new_val_2'], inplace=True) # Replace original value 1 with new value 1, and original value 2 with new value 2
 df['col_name'].replace({'ori_val_1':'new_val_1', 'ori_val_2':'new_val_2'}, inplace=True)
+df.replace({'col_name_1': {'ori_val_1':'new_val_1', 'ori_val_2':'new_val_1', 'ori_val_3':'new_val_2', 'ori_val_4':'new_val_1'}}, 
+           {'col_name_2': {'ori_val_1':'new_val_1', 'ori_val_2':'new_val_1', 'ori_val_3':'new_val_2', 'ori_val_4':'new_val_1'}},
+           inplace=True)
