3bb8bf94676593d2d8e2bcd44ec6d2386d457e99
Anni <90419301+ScienceComputing@users.noreply.github.com>
2023-12-24 19:39:25 -0500

    Update Iterate_Row.py

--- Utilities/pandas/Iterate_Row.py
+++ Utilities/pandas/Iterate_Row.py
@@ -17,3 +17,34 @@ for idx, vals in df_generator:
     if (idx%2) != 0:
         val_sum = sum([vals[0], vals[1], vals[2], vals[3], vals[4]])/5
         print(val_sum)
+
+
+# Reference: https://www.health.harvard.edu/heart-health/a-look-at-diastolic-blood-pressure
+# Select patients with Stage 1/2 hypertension using .iterrows()
+data = {'Patient ID': [1, 2, 3, 4, 5],
+        'Age': [35, 42, 28, 50, 62],
+        'Gender': ['M', 'F', 'M', 'F', 'F'],
+        'Diagnosis': ['Diabetes', 'Hypertension', 'Asthma', 'Cancer', 'Arthritis'],
+        'BMI': [25.5, 22.3, 28.1, 20.2, 31.7],
+        'Systolic Blood Pressure': [130, 120, 145, 110, 155],
+        'Diastolic Blood Pressure': [80, 70, 90, 65, 100],
+        'Cholesterol': [200, 180, 240, 160, 220],
+        'Smoker': [True, False, True, False, False]}
+
+df = pd.DataFrame(data)
+
+stage1_hypertension_patients = []
+stage2_hypertension_patients = []
+for _, row in df.iterrows():
+  systolic_bp = row['Systolic Blood Pressure']
+  diastolic_bp = row['Diastolic Blood Pressure']
+  if (130 <= systolic_bp <= 139) or (80 <= diastolic_bp <= 89):
+    stage1_hypertension_patients.append(row['Patient ID'])
+  elif (systolic_bp >= 140) or (diastolic_bp >= 90):
+    stage2_hypertension_patients.append(row['Patient ID'])
+
+print("Patients with Stage 1 Hypertension have the following IDs:")
+print(stage1_hypertension_patients) # [1]
+print("Patients with Stage 2 Hypertension have the following IDs:")
+print(stage2_hypertension_patients) # [3, 5]
+
