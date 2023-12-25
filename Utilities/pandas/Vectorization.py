89f072b6ffeb912ed06e4be6bc927f6b099e8d34
Anni <90419301+ScienceComputing@users.noreply.github.com>
2023-12-25 15:03:39 -0500

    Update Vectorization.py

--- Utilities/pandas/Vectorization.py
+++ Utilities/pandas/Vectorization.py
@@ -1,5 +1,7 @@
 # Calculate the mean value of 5 columns in each row
-mean_row = df[['Column1', 'Column2', 'Column3', 'Column4', 'Column5']].mean(axis=1)
+mean_row = df[['Column1', 'Column2', 'Column3', 'Column4', 'Column5']].mean(axis=1) # Return a pandas series
+mean_row_2 = df[['Column1', 'Column2', 'Column3', 'Column4', 'Column5']].values.mean(axis=1) # Return a ndarray; a bit faster than the above approach
 
-# Calculate the mean of each of the 5 columns in all rows
+# Calculate the mean of each of the 5 columns across all rows
 mean_col = df[['Column1', 'Column2', 'Column3', 'Column4', 'Column5']].mean(axis=0)
+mean_col_2 = df[['Column1', 'Column2', 'Column3', 'Column4', 'Column5']].values.mean(axis=0)
