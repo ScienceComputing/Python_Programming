173e4ad4b990972eef859a413adafc4a34c56fed
Anni <90419301+ScienceComputing@users.noreply.github.com>
2023-12-27 10:11:43 -0500

    Update Replace_Value.py

--- Utilities/pandas/Replace_Value.py
+++ Utilities/pandas/Replace_Value.py
@@ -3,7 +3,7 @@ import pandas as pd
 # Reference: https://pandas.pydata.org/docs/reference/api/pandas.DataFrame.replace.html#pandas.DataFrame.replace
 df['col_name'].replace('ori_val', 'new_val', inplace=True)
 df['col_name'].replace(['ori_val_1', 'ori_val_2'], 'new_val', inplace=True)
-"""Replace missing values with NA in the disease column"""
+"""Replace missing values with NaN (not a number) in the disease column"""
 df.disease.replace(['Unknown', 'Unk', 'UNK', 'None', 'none', 'missing', 'No', 'no'], np.nan, inplace=True)
 
 df['col_name'].replace(['ori_val_1', 'ori_val_2'], ['new_val_1', 'new_val_2'], inplace=True) # Replace original value 1 with new value 1, and original value 2 with new value 2
