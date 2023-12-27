5a1c1eec6bee18662de5526d0ee8e13a7479573a
Anni <90419301+ScienceComputing@users.noreply.github.com>
2023-12-27 11:09:45 -0500

    Update Alter_Data_Type.py

--- Utilities/pandas/Alter_Data_Type.py
+++ Utilities/pandas/Alter_Data_Type.py
@@ -1,1 +1,7 @@
 df['feature'] = df.feature.astype(float)
+
+df.drug_class.unique() # View the unique values in the drug_class column
+df['drug_class'] = df.drug_class.astype('category')
+
+df['diagnosis_time'] = pd.to_datetime(df.diagnosis_time) # Convert diagnosis_time to datetime
+df.info() # Inspect the cleaned dataset
