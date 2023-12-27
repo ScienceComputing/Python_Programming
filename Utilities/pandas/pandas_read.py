3afc53b2e90fb11cf379da419bede263eb6bdbe9
Anni <90419301+ScienceComputing@users.noreply.github.com>
2023-12-27 10:02:53 -0500

    Update pandas_read.py

--- Utilities/pandas/pandas_read.py
+++ Utilities/pandas/pandas_read.py
@@ -3,5 +3,8 @@ pd.read_excel()
 pd.read_json()
 pd.read_clipboard()
 
-# Get a glimpse of 10 values randomly selected in a column named disease
-df.disease.sample(n=10)
+# Get a glimpse of 20 values randomly selected in a column named disease
+df.disease.sample(n=20)
+
+# Get a glimpse of 20 values randomly selected of the disease column that are not 'Norovirus'
+df[df.disease != 'Norovirus'].disease.sample(n=20)
