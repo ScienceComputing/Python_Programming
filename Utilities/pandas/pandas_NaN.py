e7ef63d56926e5ba637ef4ff434d9b50082ad2f6
Anni <90419301+ScienceComputing@users.noreply.github.com>
2023-12-27 11:03:21 -0500

    Update pandas_NaN.py

--- Utilities/pandas/pandas_NaN.py
+++ Utilities/pandas/pandas_NaN.py
@@ -1,5 +1,6 @@
 # Topic: missing values (NaN)
 # Motivation: some python functions ignore missing data by default, and some functions cannot handle the missing data
+
 # Detect the missing values
 df.isna() # Return a Boolean for every single element in the dataset
 df.isna().any() # Return a Boolean for every column in the dataset
@@ -13,9 +14,12 @@ plot.show()
 # Remove the missing values
 df.dropna() # Remove the rows that contain missing values
 
-# Replace the missing values 
+# Replace the missing values with 0 
 df.fillna(0) # Notice the distribution of variables with the missing values may change after replacing the missing values with 0
 df["numerical_var_before_replace"].hist(bins=20)
 plt.show()
 df["numerical_var_after_replace"].hist(bins=20)
 plt.show()
+
+# Replace originally coded missing values with NaN (not a number) in the disease column
+df.disease.replace(['Unknown', 'Unk', 'UNK', 'None', 'none', 'missing', 'No', 'no'], np.nan, inplace=True)
