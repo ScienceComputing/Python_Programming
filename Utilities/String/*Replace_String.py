76aa56f54edc87c88641ec24aaa76eb285082b9f
Anni <90419301+ScienceComputing@users.noreply.github.com>
2023-12-27 09:57:54 -0500

    Update and rename Replace_String.py to *Replace_String.py

--- Utilities/String/*Replace_String.py
+++ Utilities/String/*Replace_String.py
@@ -2,3 +2,9 @@
 str1 = 'Cysteine Cysteine helps prevent side effects due to drug reactions and toxic chemicals'
 str1.replace('Cysteine ', '', 1)
 # 'Cysteine helps prevent side effects due to drug reactions and toxic chemicals'
+
+
+"""Clean the values in a feature of a ML dataset"""
+# Remove ICD-10-CM code from the disease column
+df_train['disease'] = df_train['disease'].str.replace('A15-A19:', '')
+df_train
