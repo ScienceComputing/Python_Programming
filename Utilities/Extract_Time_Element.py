1bbe8e7cd2ff90d2ef3f0e2243fa6535fe4ada59
Anni <90419301+ScienceComputing@users.noreply.github.com>
2023-12-27 11:24:12 -0500

    Update Extract_Time_Element.py

--- Utilities/Extract_Time_Element.py
+++ Utilities/Extract_Time_Element.py
@@ -1,3 +1,4 @@
+"""Case 1"""
 from datetime import datetime
 class ExtractTimeElement:    
     def __init__(self, year, month, day):
@@ -22,3 +23,10 @@ target_time_2 = ExtractTimeElement.from_datetime(datetime.today())
 print(target_time_2.year) 
 print(target_time_2.month)
 print(target_time_2.day)
+
+"""Case 2"""
+hours = df['diagnosis_time'].str.extract('(\d+) hr').fillna(0).astype(int) # fill NaN with 0
+mins = df['diagnosis_time'].str.extract('(\d+) min').fillna(0).astype(int)
+df['diagnosis_time_mins'] = hours * 60 + mins
+df[['time_mins']]
+
