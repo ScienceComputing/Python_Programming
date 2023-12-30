b7d60a64b27229c852b812ec188870542b2229c7
Anni <90419301+ScienceComputing@users.noreply.github.com>
2023-12-30 10:06:33 -0500

    Update multiprocessing.py

--- ParallelProgramming/multiprocessing.py
+++ ParallelProgramming/multiprocessing.py
@@ -1,1 +1,33 @@
-# TD
+# Reference: https://github.com/ScienceComputing/GSEApy
+
+import pandas as pd
+import gseapy as gp
+from gseapy import Msigdb
+from multiprocessing import Pool
+import os
+msig = Msigdb()
+
+def run_gsea(input_file, output_file):
+    data = pd.read_csv(input_file, index_col=0)
+    # Ensure genes are in Entrez IDs:
+    data.index = data.index.map(gp.get_geneset(data.index, organism='hsapiens', gene_type='entrez'))
+    genes_of_interest = list(data.index)  
+    pathway_database = Msigdb().get_gmt(category='mh.all', dbver="2023.1.Mm")  # Mouse hallmark gene sets
+    gsea_results = gp.gsea(data=genes_of_interest,
+                           gene_sets=pathway_database,
+                           permutation_num=1000,
+                           outdir=output_file_dir,
+                           no_plot=True,
+                           method='s2n', # signal_to_noise
+                          )
+    gsea_results.res2d.to_csv(output_file, sep='\t')
+
+data_dir = '/Users/your_name/scRNAseq/data/'
+result_dir = '/Users/your_name/scRNAseq/result/'
+
+data_files = [data_dir + filename for filename in os.listdir(data_dir)]
+result_files = [result_dir + "out_" + filename for filename in os.listdir(data_dir)]
+
+# Parallel processing:
+with Pool(16) as p:
+    results = p.map(process_file, zip(data_files, result_files))
