b4b7df4759788c53905f40ed532ad7cff988d0fa
Anni <90419301+ScienceComputing@users.noreply.github.com>
2023-12-31 12:30:55 -0500

    Update Open_CSV.py

--- Utilities/Open_CSV.py
+++ Utilities/Open_CSV.py
@@ -8,6 +8,8 @@ with open('variable_dictionary.csv') as f:
         # f1_val, f2_val = line.rstrip().split(','); no need to write this anymore
         features.append({"variable": f1_val, "code": f2_val})
 
+for feature in sorted(features, key=lambda feature : feature['variable']): 
+    print(f"{feature['variable']} is coded as follows: {feature['code']}")
 
 # If a CSV file more than 2 columns
 features = []
@@ -15,3 +17,6 @@ with open('variable_dictionary.csv') as f:
     r = csv.reader(f)
     for row in f: # this file has more than 2 columns
         features.append({"variable": row[0], "code": row[2]})
+
+for feature in sorted(features, key=lambda feature : feature['variable']): 
+    print(f"{feature['variable']} is coded as follows: {feature['code']}")
