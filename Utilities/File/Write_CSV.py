fe4f83a6a988123993ac44f36920618aea078102
Anni <90419301+ScienceComputing@users.noreply.github.com>
2023-12-31 13:14:46 -0500

    Update Write_CSV.py

--- Utilities/File/Write_CSV.py
+++ Utilities/File/Write_CSV.py
@@ -6,3 +6,12 @@ code = input("How is the variable of your interest coded?")
 with open("variable_dictionary", "a") as f: # a: append mode, no overwrite
     w = csv.writer(f)
     w.writerow([variable, code])
+
+
+"""If a CSV file has pre-defined column headers - variable name, code"""
+variable = input("What's the variable of your interest?")
+code = input("How is the variable of your interest coded?")
+
+with open("variable_dictionary", "a") as f: 
+    w = csv.DictWriter(f, fieldname=['variable name', 'code'])
+    w.writerow({'variable name': variable, 'code': code}) # Or w.writerow({'code': code, 'variable name': variable})
