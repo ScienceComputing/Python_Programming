f80584b9ac52198d7bf2e670f61be4e28d7c9ea5
Anni <90419301+ScienceComputing@users.noreply.github.com>
2023-12-31 12:20:49 -0500

    Update Open_File.py

--- Utilities/Open_File.py
+++ Utilities/Open_File.py
@@ -1,7 +1,8 @@
 # Reference:
 # https://github.com/ScienceComputing/Python_Programming/blob/main/Utilities/String/*Remove_Character.py
 # https://github.com/ScienceComputing/Python_Programming/blob/main/Python_Overview.ipynb
-# https://docs.python.org/3/howto/sorting.html
+# https://docs.python.org/3/library/functions.html#sorted
+# sorted(iterable, /, *, key=None, reverse=False)
 # https://github.com/ScienceComputing/Python_Programming/blob/main/Utilities/Lambda_Function.py
 
 """
