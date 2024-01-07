2f8cc103f044d6096266702302460bcdaa0fd0de
Anni <90419301+ScienceComputing@users.noreply.github.com>
2024-01-07 10:56:28 -0500

    Update Stack_Linear_Layer.py

--- Artificial_Intelligence/PyTorch/Stack_Linear_Layer.py
+++ Artificial_Intelligence/PyTorch/Stack_Linear_Layer.py
@@ -7,8 +7,19 @@ input_tensor = torch.from_numpy(DE_data)
 pred_model = nn.Sequential(
     nn.Linear(in_features=input_tensor.shape[1], out_features=100), 
     nn.Linear(in_features=100, out_features=70), 
-    nn.Linear(in_features=70, out_features=30)  # 30 predictions for the phenotype classification
+    nn.Linear(in_features=70, out_features=30)  # 30 predictions for the binary phenotype classification
+    nn.Sigmoid()
 )
 
 pred_result = pred_model(input_tensor)
 print(pred_result)
+
+pred_model_2 = nn.Sequential(
+    nn.Linear(in_features=input_tensor.shape[1], out_features=100), 
+    nn.Linear(in_features=100, out_features=70), 
+    nn.Linear(in_features=70, out_features=30)  # 30 predictions for the multi-class phenotype classification
+    nn.Softmax(dim=-1)
+)
+
+pred_result_2 = pred_model_2(input_tensor)
+print(pred_result_2)
