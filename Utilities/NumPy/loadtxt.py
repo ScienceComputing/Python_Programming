42a8bdf4027aec4da6e306a7aa8eb8ab7ab459b6
Anni <90419301+ScienceComputing@users.noreply.github.com>
2024-01-07 11:21:09 -0500

    Update loadtxt.py

--- Utilities/NumPy/loadtxt.py
+++ Utilities/NumPy/loadtxt.py
@@ -1,8 +1,9 @@
-# Reference: https://fr.wikipedia.org/wiki/Variant_Call_Format#/media/Fichier:Binary_BCF_versus_VCF_format.png
-# Reference: https://github.com/ScienceComputing/Python_Programming/blob/main/Utilities/NumPy/Create_Reshape_Array.py
+# Reference: 
+# https://fr.wikipedia.org/wiki/Variant_Call_Format#/media/Fichier:Binary_BCF_versus_VCF_format.png
+# https://github.com/ScienceComputing/Python_Programming/blob/main/Utilities/NumPy/Create_Reshape_Array.py
 
 import numpy as np
 
-gene_expression_data = np.loadtxt("gene_expression.csv", delimiter=",", skiprows=1)
-dna_sequences = np.loadtxt("dna_sequences.fasta", dtype="str")
-vcf_data = np.loadtxt("variants.vcf", dtype="str", skiprows=7, usecols=range(9), delimiter="\t")
+gene_expression = np.loadtxt("gene_expression.csv", delimiter=",", skiprows=1)
+dna_sequence = np.loadtxt("dna_sequences.fasta", dtype="str")
+vcf = np.loadtxt("variants.vcf", dtype="str", skiprows=7, usecols=range(9), delimiter="\t")
