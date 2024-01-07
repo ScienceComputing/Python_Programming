47eed113ae4f0bf1950be67d42e5ad6c424ab645
Anni <90419301+ScienceComputing@users.noreply.github.com>
2024-01-07 11:10:53 -0500

    Create loadtxt.py

---
+++ Utilities/NumPy/loadtxt.py
@@ -0,0 +1,7 @@
+# Reference: https://fr.wikipedia.org/wiki/Variant_Call_Format#/media/Fichier:Binary_BCF_versus_VCF_format.png
+
+import numpy as np
+
+gene_expression_data = np.loadtxt("gene_expression.csv", delimiter=",", skiprows=1)
+dna_sequences = np.loadtxt("dna_sequences.fasta", dtype="str")
+vcf_data = np.loadtxt("variants.vcf", skiprows=7, usecols=range(9), delimiter="\t")
