8f1d7905096adc542f735e52f111c87866866d77
Anni <90419301+ScienceComputing@users.noreply.github.com>
2024-03-19 21:14:23 -0400

    Update Initialize_Layer.py

--- Artificial_Intelligence/PyTorch/Initialize_Layer.py
+++ Artificial_Intelligence/PyTorch/Initialize_Layer.py
@@ -2,3 +2,9 @@
 import torch.nn as nn
 layer = nn.Linear(in_features=9, out_features=27)
 print(layer.weight.min(), layer.weight.max())
+
+# Initialize layer weights with the uniform distribution
+# To make sure the outputs of our layer stay small, it's important to the layer weights on the lower side
+layer = nn.Linear(64, 128)
+nn.init.uniform_(layer.weight) 
+print(layer.weight.min(), layer.weight.max())
