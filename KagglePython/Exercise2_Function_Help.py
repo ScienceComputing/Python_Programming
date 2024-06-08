0688168bd0fcb8c090b90ba0cde3e99cd8e43020
Anni <90419301+ScienceComputing@users.noreply.github.com>
2024-06-08 15:07:23 -0400

    Update Exercise2_Function_Help.py

--- KagglePython/Exercise2_Function_Help.py
+++ KagglePython/Exercise2_Function_Help.py
@@ -29,3 +29,14 @@ round(338424, ndigits = -3)
 # We probably don't care whether it's really 338,424, or 338,425, or 338,177. All those digits of accuracy are just distracting. We can chop them off by calling round() with ndigits=-3:
 # The area of Finland is 338,000 km²
 # The area of Greenland is 2,166,000 km²
+
+## Question 3
+# Below is a simple function that will calculate the number of candies to smash for any number of total candies. Modify it so that it optionally takes a second argument representing the number of friends the candies are being split between. If no second argument is provided, it should assume 3 friends, as before.
+def to_smash(total_candies, num_friends=3):
+    """Return the number of leftover candies that must be smashed after distributing
+    the given number of candies evenly between 3 friends.
+    
+    >>> to_smash(91)
+    1
+    """
+    return total_candies % num_friends
