158d3b769bb8e6c2bc4afc4145151a635a8ec0e6
Anni <90419301+ScienceComputing@users.noreply.github.com>
2024-06-08 15:34:00 -0400

    Create Exercise3.py

---
+++ KagglePython/Exercise3.py
@@ -0,0 +1,15 @@
+## Question 1
+pi = 3.14159 # approximate
+diameter = 3
+
+# Create a variable called 'radius' equal to half the diameter
+radius = diameter / 2
+
+# Create a variable called 'area', using the formula for the area of a circle: pi times the radius squared
+area = pi * radius ** 2
+
+## Question 2
+a = [1, 2, 3]
+b = [3, 2, 1]
+
+a, b = b, a
