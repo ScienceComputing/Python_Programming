c77e5f3ae56ce4a7615f0b98232a9fa7318694f9
Anni <90419301+ScienceComputing@users.noreply.github.com>
2024-06-08 15:04:45 -0400

    Update Exercise2_Function_Help.py

--- KagglePython/Exercise2_Function_Help.py
+++ KagglePython/Exercise2_Function_Help.py
@@ -11,7 +11,7 @@ def round_to_two_places(num):
     >>> round_to_two_places(3.14159)
     3.14
     """
-    return(round(num, 2)) # return
+    return round(num, 2) # return
     # "pass" is a keyword that does literally nothing. We used it as a placeholder because after we begin a code block, Python requires at least one line of code.
     pass
 
