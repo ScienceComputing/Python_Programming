720b0292db27859ecd04633674bf6c8b82b59e45
Anni <90419301+ScienceComputing@users.noreply.github.com>
2024-06-10 09:29:47 -0400

    Update Exercise_Booleans_Conditionals.py

--- KagglePython/Exercise_Booleans_Conditionals.py
+++ KagglePython/Exercise_Booleans_Conditionals.py
@@ -6,3 +6,21 @@ def sign(num=-1):
         return 0
     else:
         return 1
+
+def to_smash(total_candies):
+    """Return the number of leftover candies that must be smashed after distributing
+    the given number of candies evenly between 3 friends.
+    
+    >>> to_smash(91)
+    1
+    """
+    if total_candies <= 1:
+        print("Splitting", total_candies, "candy")
+    else:
+        print("Splitting", total_candies, "candies")
+    # Use a conditional expression
+    # print("Splitting", total_candies, "candy" if total_candies == 1 else "candies")
+    return total_candies % 3
+
+to_smash(91)
+to_smash(1)
