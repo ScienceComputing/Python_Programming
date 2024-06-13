62540e1cad1722a301e2c0c2d4c52e921e3c973a
Anni <90419301+ScienceComputing@users.noreply.github.com>
2024-06-13 19:11:35 -0400

    Update Exercise_Booleans_Conditionals.py

--- KagglePython/Exercise_Booleans_Conditionals.py
+++ KagglePython/Exercise_Booleans_Conditionals.py
@@ -115,4 +115,4 @@ def exactly_one_topping(ketchup, mustard, onion):
     """Return whether the customer wants exactly one of the three available toppings
     on their hot dog.
     """
-    return (int(ketchup) + int(mustard) + int(onion)) == 1
+    return (int(ketchup) + int(mustard) + int(onion)) == 1 # return (ketchup + mustard + onion) == 1
