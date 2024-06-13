c60eb8cba69a6f2f73a807ddeb02d067a517b23a
Anni <90419301+ScienceComputing@users.noreply.github.com>
2024-06-13 17:03:58 -0400

    Update Exercise_Booleans_Conditionals.py

--- KagglePython/Exercise_Booleans_Conditionals.py
+++ KagglePython/Exercise_Booleans_Conditionals.py
@@ -94,3 +94,5 @@ def wants_plain_hotdog(ketchup, mustard, onion):
     """Return whether the customer wants a plain hot dog with no toppings.
     """
     return not (ketchup or mustard or onion)
+
+## Question 5c
