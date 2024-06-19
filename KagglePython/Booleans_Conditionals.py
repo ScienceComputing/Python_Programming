f37a1c5fc3009510c816148f51af72a7d6f96c8d
Anni <90419301+ScienceComputing@users.noreply.github.com>
2024-06-19 12:27:43 -0400

    Create Booleans_Conditionals.py

---
+++ KagglePython/Booleans_Conditionals.py
@@ -0,0 +1,16 @@
+# Ref: https://www.kaggle.com/code/colinmorris/booleans-and-conditionals
+var = True
+print(var) # True
+print(type(var)) # <class 'bool'>
+
+# Get boolean values from boolean operators
+def is_suitable_for_atac_seq(fragment_length):
+    """
+    Is the given fragment length suitable for ATAC-Seq analysis?
+    ATAC-Seq typically uses fragments between 50 and 150 bp.
+    """
+    return 50 <= fragment_length <= 150
+
+print("Is a 30 bp fragment suitable for ATAC-Seq?", is_suitable_for_atac_seq(30)) # False
+print("Is a 100 bp fragment suitable for ATAC-Seq?", is_suitable_for_atac_seq(100)) # True
+print("Is a 200 bp fragment suitable for ATAC-Seq?", is_suitable_for_atac_seq(200)) # True
