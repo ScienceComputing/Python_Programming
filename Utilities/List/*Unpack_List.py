be7be81a06cbafdb021d549ba86d4853f67742ce
Anni <90419301+ScienceComputing@users.noreply.github.com>
2024-07-21 22:09:13 -0400

    Update *Unpack_List.py

--- Utilities/List/*Unpack_List.py
+++ Utilities/List/*Unpack_List.py
@@ -11,6 +11,10 @@ shares
 price
 year
 
+a, b, *_ = ['a', 'cde', 6, 9, 10]
+print(_)
+# [6, 9, 10]
+
 line = 'https://ftp.ncbi.nlm.nih.gov/geo/series/GSE85nnn/GSE85241/suppl/GSE85241%5Fcellsystems%5Fdataset%5F4donors%5Fupdated%2Ecsv%2Egz'
 ncbi, base_name = line.split('suppl/')
 base_name
