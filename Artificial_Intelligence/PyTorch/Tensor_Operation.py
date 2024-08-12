6223e6c6a292bcdc4d1e5792cc109ee854a77daf
Anni <90419301+ScienceComputing@users.noreply.github.com>
2024-08-12 07:24:21 -0400

    Update Tensor_Operation.py

--- Artificial_Intelligence/PyTorch/Tensor_Operation.py
+++ Artificial_Intelligence/PyTorch/Tensor_Operation.py
@@ -24,7 +24,22 @@ data_1 * data_2
 #         [ 6, 18, 18]])
 
 # Transposition
+data_1.T
 
 # Matrix multiplication
+data_1 = torch.tensor([[1, 5, 6], [2, 6, 9]])
+data_2_t = torch.tensor([[0, 1, 1], [3, 3, 2]]).T
+data_1 @ data_2_t
 
 # Concatenation
+# Concatenate along the rows
+torch.cat((data_1, data_2), dim=0)
+# tensor([[1, 5, 6],
+#         [2, 6, 9],
+#         [0, 1, 1],
+#         [3, 3, 2]])
+
+# Concatenate along the columns
+torch.cat((data_1, data_2), dim=1)
+# tensor([[1, 5, 6, 0, 1, 1],
+#         [2, 6, 9, 3, 3, 2]])
